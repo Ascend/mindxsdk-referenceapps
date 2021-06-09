6aa04df089eebec0f35c7222b03ad9491d507a4b
wwwwuji <1030685104@qq.com>
2021-06-09 10:25:19 +0800

    ocr

---
+++ contrib/OCR/KeywordDetection/KeywordDetection.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Copyright 2020 Huawei Technologies Co., Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# import StreamManagerApi.py
+import MxpiDataType_pb2 as MxpiDataType
+from StreamManagerApi import *
+import json
+import cv2
+import numpy as np
+if __name__ == '__main__':
+    pipeline_path = "../pipeline/KeywordDetection.pipeline"
+    streamName = b'KeywordDetection'
+    
+    
+
+    streamManagerApi = StreamManagerApi()
+    ret = streamManagerApi.InitManager()
+    if ret != 0:
+        print("Failed to init Stream manager, ret=%s" % str(ret))
+        exit()
+
+    # create streams by pipeline config file
+    with open(pipeline_path, 'rb') as f:
+        pipelineStr = f.read()
+    ret = streamManagerApi.CreateMultipleStreams(pipelineStr)
+    if ret != 0:
+        print("Failed to create Stream, ret=%s" % str(ret))
+        exit()
+        
+    inPluginId = 0    
+    # Construct the input of the stream
+    dataInput = MxDataInput()
+    img_path = "../data/en_text/1.jpg"
+    
+    
+    with open(img_path, 'rb') as f:
+        dataInput.data = f.read()
+    inPluginId = 0
+    uniqueId = streamManagerApi.SendData(streamName, inPluginId, dataInput)
+    if uniqueId < 0:
+        print("Failed to send data to stream.")
+        exit()
+    
+    
+    inPluginId_key = 1
+    #key
+    key_file = open("./bert_key.txt", 'r')
+    key_dict = []
+    
+    for key in key_file.readlines():
+        key_dict.append(key.strip())  
+    
+    mxpiTextsInfoList_key = MxpiDataType.MxpiTextsInfoList()
+    textsInfoVec_key = mxpiTextsInfoList_key.textsInfoVec.add()
+    
+    for key in key_dict:
+        textsInfoVec_key.text.append(key)
+        
+    key1 = b'appsrc1'
+    protobufVec = InProtobufVector()
+    protobuf_key = MxProtobufIn()
+    protobuf_key.key = key1
+    protobuf_key.type = b'MxTools.MxpiTextsInfoList'
+    protobuf_key.protobuf = mxpiTextsInfoList_key.SerializeToString()
+    protobufVec.push_back(protobuf_key)
+    
+
+    uniqueId = streamManagerApi.SendProtobuf(streamName, inPluginId_key, protobufVec)
+    if uniqueId < 0:
+        print("Failed to send data to stream.")
+        exit()
+
+    keyVec = StringVector()
+    keyVec.push_back(b'appsrc0')
+    inferResult = streamManagerApi.GetProtobuf(streamName, inPluginId, keyVec)
+    if inferResult.size() == 0:
+        print("inferResult is null")
+        exit()
+    if inferResult[0].errorCode != 0:
+        print("GetProtobuf error. errorCode=%d" % (
+            inferResult[0].errorCode))
+        exit()
+    streamManagerApi.DestroyAllStreams()
---
+++ contrib/OCR/plugins/TextInfoPlugin/TextInfoPlugin.h
@@ -0,0 +1,115 @@
+#ifndef MXPLUGINS_TEXTINFOPLUGIN_H
+#define MXPLUGINS_TEXTINFOPLUGIN_H
+
+#include "MxBase/ErrorCode/ErrorCode.h"
+#include "MxTools/PluginToolkit/base/MxPluginGenerator.h"
+#include "MxTools/PluginToolkit/base/MxPluginBase.h"
+#include "MxTools/PluginToolkit/buffer/MxpiBufferManager.h"
+#include "MxTools/PluginToolkit/metadata/MxpiMetadataManager.h"
+#include "MxTools/Proto/MxpiDataType.pb.h"
+#include <string>
+#include "MxBase/Log/Log.h"
+#include "opencv2/opencv.hpp"
+#include "MxBase/MemoryHelper/MemoryHelper.h"
+#include "MxBase/DvppWrapper/DvppWrapper.h"
+#include "MxBase/Tensor/TensorBase/TensorBase.h"
+#include "MxStream/StreamManager/MxStreamManager.h"
+#include "MxTools/PluginToolkit/MxpiDataTypeWrapper/MxpiDataTypeDeleter.h"
+#include "MxTools/Proto/MxpiDataType.pb.h"
+#include <chrono>
+
+using namespace std;
+
+/**
+ * This plugin is to stack frames based on detected objects.
+*/
+
+namespace MxPlugins {
+    class TextInfoPlugin : public MxTools::MxPluginBase {
+    public:
+        /**
+        * @description: Init configs.
+        * @param configParamMap: config.
+        * @return: APP_ERROR.
+        */
+        APP_ERROR Init(std::map<std::string, std::shared_ptr<void>>& configParamMap) override;
+
+        /**
+        * @description: DeInit device.
+        * @return: Error code.
+        */
+        APP_ERROR DeInit() override;
+
+        /**
+        * @description: MxpiFairmot plugin process.
+        * @param mxpiBuffer: data receive from the previous.
+        * @return: Error code.
+        */
+        APP_ERROR Process(std::vector<MxTools::MxpiBuffer*> &mxpiBuffer) override;
+
+        /**
+        * @description: MxpiFairmot plugin define properties.
+        * @return: properties.
+        */
+        static std::vector<std::shared_ptr<void>> DefineProperties();
+
+        /**
+        * @api
+        * @brief Define the number and data type of input ports.
+        * @return MxTools::MxpiPortInfo.
+        */
+        static MxTools::MxpiPortInfo DefineInputPorts();
+
+        /**
+        * @api
+        * @brief Define the number and data type of output ports.
+        * @return MxTools::MxpiPortInfo.
+        */
+        static MxTools::MxpiPortInfo DefineOutputPorts();
+        std::string ltrim(std::string str);
+        std::string rtrim(std::string str);
+        std::string trim(std::string str);
+        std::vector<std::string> split(const std::string& str, char delimiter);
+        std::vector<std::string> whitespace_tokenize(std::string text);
+
+        bool _is_whitespace(char letter);
+        bool _is_punctuation(char letter);
+        bool _is_control(char letter);
+
+        std::map<std::string, int> read_vocab(const char* filename);
+        std::string _clean_text(std::string text);
+        vector<std::string> _run_split_on_punc(std::string text);
+        vector<std::string> tokenize1(std::string text);
+        void add_vocab1(map<std::string, int> vocab);
+        vector<std::string> tokenize2(std::string text);
+
+        void add_vocab2(const char* vocab_file);
+        void encode(vector<std::string> tokens_A, vector<float>& input_ids, vector<float>& input_mask, vector<float>& segment_ids, int max_seq_length , map<string, int> vocab, int maxlen_);
+
+        vector<float> convert_tokens_to_ids(vector<std::string> tokens, map<string, int> vocab, int maxlen_);
+        vector<std::string> tokenize3(std::string text);
+
+
+        /**
+         * @api
+         * @brief Get the >confidence objects and responding id feature
+         * @param key
+         * @param buffer
+         * @return APP_ERROR
+         */
+    private:
+        std::string dataSource_ = "";
+        bool do_lower_case_;
+        vector<string> never_split_;
+        map<string, int> vocab_;
+        string unk_token_;
+        int max_input_chars_per_word_;
+        map<string, int> vocab;
+        map<int, string> ids_to_tokens;
+        bool do_basic_tokenize_;
+        int maxlen_;
+
+    };
+}
+
+#endif
