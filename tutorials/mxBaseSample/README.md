7bbe819f941de950750fcf401e4297224211be4a
tanghongyan <1349905607@qq.com>
2021-07-08 20:29:51 +0800

    本次提交新增mxBase图像检测样例其中还包含后处理模块开发样例

---
+++ tutorials/mxBaseSample/README.md
@@ -0,0 +1,53 @@
+
+# C++ 基于MxBase 的yolov3图像检测样例及yolov3的后处理模块开发
+
+## 介绍
+本开发样例是基于mxBase开发的端到端推理的C++应用程序，可在昇腾芯片上进行 yolov3 目标检测，并把可视化结果保存到本地。其中包含yolov3的后处理模块开发。
+该Sample的主要处理流程为：
+Init > ReadImage >Resize > Inference >PostProcess >DeInit
+
+## 模型转换
+
+**步骤1** 模型获取
+在ModelZoo上下载YOLOv3模型 ，选择“历史版本”中版本1.1下载。[下载地址](https://www.hiascend.com/zh/software/modelzoo/detail/C/210261e64adc42d2b3d84c447844e4c7)
+**步骤2** 模型存放
+将获取到的YOLOv3模型pb文件放至上一级的models文件夹中
+**步骤3** 执行模型转换命令
+
+(1) 配置环境变量
+#### 设置环境变量（请确认install_path路径是否正确）
+#### Set environment PATH (Please confirm that the install_path is correct).
+```c
+export install_path=/usr/local/Ascend/ascend-toolkit/latest
+export PATH=/usr/local/python3.7.5/bin:${install_path}/atc/ccec_compiler/bin:${install_path}/atc/bin:$PATH
+export PYTHONPATH=${install_path}/atc/python/site-packages:${install_path}/atc/python/site-packages/auto_tune.egg/auto_tune:${install_path}/atc/python/site-packages/schedule_search.egg:$PYTHONPATH
+export LD_LIBRARY_PATH=${install_path}/atc/lib64:$LD_LIBRARY_PATH
+export ASCEND_OPP_PATH=${install_path}/opp
+
+```
+(2) 转换模型
+```
+atc --model=./yolov3_tf.pb --framework=3 --output=./yolov3_tf_aipp --output_type=FP32 --soc_version=Ascend310 --input_shape="input/input_data:1,416,416,3" --log=info --insert_op_conf=./yolov3_tf_aipp.cfg --out_nodes="conv_lbbox/BiasAdd:0;conv_mbbox/BiasAdd:0;conv_sbbox/BiasAdd:0"
+```
+
+## 编译与运行
+**步骤1** 修改CMakeLists.txt文件 将set(MX_SDK_HOME ${SDK安装路径}) 中的${SDK安装路径}替换为实际的SDK安装路径
+
+**步骤2** 设置环境变量
+ASCEND_HOME Ascend安装的路径，一般为/usr/local/Ascend
+LD_LIBRARY_PATH 指定程序运行时依赖的动态库查找路径，包括ACL，开源软件库，libmxbase.so以及libyolov3postprocess.so的路径
+```
+export ASCEND_HOME=/usr/local/Ascend
+export ASCEND_VERSION=nnrt/latest
+export ARCH_PATTERN=.
+export LD_LIBRARY_PATH=${MX_SDK_HOME}/lib/modelpostprocessors:${MX_SDK_HOME}/lib:${MX_SDK_HOME}/opensource/lib:${MX_SDK_HOME}/opensource/lib64:/usr/local/Ascend/driver/lib64:/usr/local/Ascend/ascend-toolkit/latest/acllib/lib64:${LD_LIBRARY_PATH}
+```
+
+**步骤3** cd到mxbase目录下，执行如下编译命令：
+bash build.sh
+
+**步骤4** 制定jpg图片进行推理，准备一张推理图片放入mxbase 目录下。eg:推理图片为test.jpg
+cd 到mxbase 目录下
+```
+./mxBase_sample ./test.jpg
+```
---
+++ tutorials/mxBaseSample/main.cpp
@@ -0,0 +1,110 @@
+/*
+ * Copyright(C) 2021. Huawei Technologies Co.,Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include <vector>
+#include <Yolov3Detection.h>
+#include "MxBase/Log/Log.h"
+
+std::vector<double> g_inferCost;
+
+void SplitString(const std::string &s, std::vector<std::string> &v, const std::string &c)
+{
+    std::string::size_type pos1, pos2;
+    pos2 = s.find(c);
+    pos1 = 0;
+    while (std::string::npos != pos2) {
+        v.push_back(s.substr(pos1, pos2 - pos1));
+
+        pos1 = pos2 + c.size();
+        pos2 = s.find(c, pos1);
+    }
+
+    if (pos1 != s.length()) {
+        v.push_back(s.substr(pos1));
+    }
+}
+
+void InitYolov3Param(InitParam &initParam)
+{
+    initParam.deviceId = 0;
+    initParam.labelPath = "../model/coco.names";
+    initParam.checkTensor = true;
+    initParam.modelPath = "../model/yolov3_tf_aipp.om";
+    initParam.classNum = 80;
+    initParam.biasesNum = 18;
+    initParam.biases = "10,13,16,30,33,23,30,61,62,45,59,119,116,90,156,198,373,326";
+    initParam.objectnessThresh = "0.001";
+    initParam.iouThresh = "0.5";
+    initParam.scoreThresh = "0.001";
+    initParam.yoloType = 3;
+    initParam.modelType = 0;
+    initParam.inputType = 0;
+    initParam.anchorDim = 3;
+}
+
+APP_ERROR ReadImagesPath(const std::string &path, std::vector<std::string> &imagesPath)
+{
+    std::ifstream inFile;
+    inFile.open(path, std::ios_base::in);
+    std::string line;
+    // Check images path file validity
+    if (inFile.fail()) {
+        LogError << "Failed to open label file: " << path;
+        return APP_ERR_COMM_OPEN_FAIL;
+    }
+    std::vector<std::string> vectorStr;
+    std::string splitStr = " ";
+    // construct label map
+    while (std::getline(inFile, line)) {
+        if (line.find('#') <= 1) {
+            continue;
+        }
+        vectorStr.clear();
+        SplitString(line, vectorStr, splitStr);
+        imagesPath.push_back(vectorStr[1]);
+    }
+
+    inFile.close();
+    return APP_ERR_OK;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc <= 1) {
+        LogWarn << "Please input image path, such as './yolov3PostProcess test.jpg'.";
+        return APP_ERR_OK;
+    }
+
+    InitParam initParam;
+    InitYolov3Param(initParam);
+    auto yolov3 = std::make_shared<Yolov3DetectionOpencv>();
+    APP_ERROR ret = yolov3->Init(initParam);
+    if (ret != APP_ERR_OK) {
+        LogError << "Yolov3DetectionOpencv init failed, ret=" << ret << ".";
+        return ret;
+    }
+
+    std::string imgPath = argv[1];
+    ret = yolov3->Process(imgPath);
+    if (ret != APP_ERR_OK) {
+        LogError << "Yolov3DetectionOpencv process failed, ret=" << ret << ".";
+        yolov3->DeInit();
+        return ret;
+    }
+    yolov3->DeInit();
+    return APP_ERR_OK;
+}
