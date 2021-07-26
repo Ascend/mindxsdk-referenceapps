90d3791ea9d0057dda98dfd0d4e820003c6a06c9
zhangdingwen <zhangdingwen@huawei.com>
2021-07-26 16:36:32 +0800

    cleancode main.py

--- tutorials/sampleCustomProto/protoRead/main.py
+++ tutorials/sampleCustomProto/protoRead/main.py
@@ -17,8 +17,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-# import StreamManagerApi.py
-from StreamManagerApi import *
+from StreamManagerApi import StreamManagerApi, MxDataInput, StringVector
 
 import sys
 sys.path.append("../../proto")
