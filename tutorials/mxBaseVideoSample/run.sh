ea10e5d4696e3488722af4ac076837395c6dcf66
dailinrui <602182685@qq.com>
2021-07-26 18:02:24 +0800

    【修改说明 Modification】run.sh文件修改 【修改人 Modifier】代林睿

--- tutorials/mxBaseVideoSample/run.sh
+++ tutorials/mxBaseVideoSample/run.sh
@@ -13,8 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-path_cur="$(dirname "$0")"
+CUR_PATH=$(cd "$(dirname "$0")"; pwd)
 
-export LD_LIBRARY_PATH="${MX_SDK_HOME}/lib":"${MX_SDK_HOME}/opensource/lib":"${MX_SDK_HOME}/opensource/lib64":"/usr/local/Ascend/ascend-toolkit/latest/acllib/lib64":"/usr/local/Ascend/driver/lib64/":"/usr/local/python3.7.5/lib":"/usr/local/ffmpeg/lib":${LD_LIBRARY_PATH}
+echo $CUR_PATH
+
+export LD_LIBRARY_PATH=${MX_SDK_HOME}/lib:${MX_SDK_HOME}/opensource/lib:${MX_SDK_HOME}/opensource/lib64:/usr/local/Ascend/ascend-toolkit/latest/acllib/lib64:/usr/local/Ascend/driver/lib64/:/usr/local/python3.7.5/lib:/usr/local/ffmpeg/lib:${LD_LIBRARY_PATH}
 
 ./stream_pull_test
