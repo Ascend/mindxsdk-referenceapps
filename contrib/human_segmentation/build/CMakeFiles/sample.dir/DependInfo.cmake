dc6cab5ca3c84ce9a58d1fa1af45812f9adfc323
惊鸿 <38373995+Artanis-j@users.noreply.github.com>
2021-08-16 19:39:34 +0800

    first commit

---
+++ contrib/human_segmentation/build/CMakeFiles/sample.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cqu_liu2/demo/main.cpp" "/home/cqu_liu2/demo/build/CMakeFiles/sample.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_GLIBCXX_USE_CXX11_ABI=0"
+  "google=mindxsdk_private"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/cqu_liu2/MindXSDK/mxVision/include"
+  "/home/cqu_liu2/MindXSDK/mxVision/opensource/include"
+  "/home/cqu_liu2/MindXSDK/mxVision/opensource/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
