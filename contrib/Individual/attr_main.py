4853dddbf9c6eb985679f6d16a6507edf7a5083a
wyz0916 <wyz_hbqhd_0916@163.com>
2021-08-17 06:14:15 +0000

    update contrib/Individual/attr_main.py.

--- contrib/Individual/attr_main.py
+++ contrib/Individual/attr_main.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     #   exit()
 
     # create streams by pipeline config file
-    with open("./Sample.pipeline", 'rb') as f:
+    with open("./pipeline/Sample.pipeline", 'rb') as f:
         pipelineStr = f.read()
     ret = streamManagerApi.CreateMultipleStreams(pipelineStr)
     if ret != 0:
@@ -37,70 +37,46 @@ if __name__ == '__main__':
     file_handle = open('img_result.txt','w')
     file_handle2 = open('test_full.txt','r')
     while 1:
-      # Construct the input of the stream
-      dataInput = MxDataInput()
-      line = file_handle2.readline()
-      # print('******************************************************')
-      # print('type,line',type(line),line[0:34])
-      img_path = line[0:34]
-      with open(img_path, 'rb') as f:
-          dataInput.data = f.read()
+        # Construct the input of the stream
+        dataInput = MxDataInput()
+        line = file_handle2.readline()
+        img_path = line[0:34]
+        with open(img_path, 'rb') as f:
+            dataInput.data = f.read()
+     
+        # Inputs data to a specified stream based on streamName.
+        streamName = b'classification+detection'
+        inPluginId = 0
+        uniqueId = streamManagerApi.SendDataWithUniqueId(streamName, inPluginId, dataInput)
+        if uniqueId < 0:
+            print("Failed to send data to stream.")
 
-      # The following is how to set the dataInput.roiBoxs
-      """
-      roiVector = RoiBoxVector()
-      roi = RoiBox()
-      roi.x0 = 100
-      roi.y0 = 100
-      roi.x1 = 200
-      roi.y1 = 200
-      roiVector.push_back(roi)
-      dataInput.roiBoxs = roiVector
-      """
-
-      # Inputs data to a specified stream based on streamName.
-      streamName = b'classification+detection'
-      inPluginId = 0
-      uniqueId = streamManagerApi.SendDataWithUniqueId(streamName, inPluginId, dataInput)
-      if uniqueId < 0:
-          print("Failed to send data to stream.")
-      #   exit()
-
-      # Obtain the inference result by specifying streamName and uniqueId.
-      inferResult = streamManagerApi.GetResultWithUniqueId(streamName, uniqueId, 3000)
-      if inferResult.errorCode != 0:
-          print("GetResultWithUniqueId error. errorCode=%d, errorMsg=%s" % (
+        # Obtain the inference result by specifying streamName and uniqueId.
+        inferResult = streamManagerApi.GetResultWithUniqueId(streamName, uniqueId, 3000)
+        if inferResult.errorCode != 0:
+            print("GetResultWithUniqueId error. errorCode=%d, errorMsg=%s" % (
               inferResult.errorCode, inferResult.data.decode()))
-      #   exit()
-      # save the confidence
-      img_list = []
-      # print the infer result
-      # print("******************************************************************")
-      print(inferResult.data.decode())
-      # print("******************************************************************")
-      dict_all = inferResult.data.decode()
-  #    print("type:",type(dict_all))
-  #    list_all = dict_all['MxpiClass']
-      # print("******************************************************************")
-      begin = 0
-      end = len(dict_all)
-      index = 0
-      for i in range(40):
-        # find confidence in dict 
-        loc = dict_all.find('confidence', begin + index, end)
-        # next confidence
-        index = loc + 12
-        # print('confidence:', dict_all[loc+12])
-        img_list.append(int(dict_all[loc+12]))
+        # save the confidence
+        img_list = []
+        # print the infer result
+        # print("******************************************************************")
+        print(inferResult.data.decode())
+        # print("******************************************************************")
+        dict_all = inferResult.data.decode()
 
-  #    print("list:",list_all)
-      # print("******************************************************************")
-      print("img_name:",img_path)
-      print("list:",img_list)
-      file_handle.write(img_path+' ')
-   #   file_handle.write(imgs[j]+' ')
-      file_handle.write(str(img_list).replace("[","").replace("]","").replace(","," "))
-      file_handle.write('\n')
-      # destroy streams
+        # print("******************************************************************")
+        begin = 0
+        end = len(dict_all)
+        index = 0
+        for i in range(40):
+          # find confidence in dict 
+          loc = dict_all.find('confidence', begin + index, end)
+          # next confidence
+          index = loc + 12
+          img_list.append(int(dict_all[loc+12]))
+        file_handle.write(img_path+' ')
+        file_handle.write(str(img_list).replace("[","").replace("]","").replace(","," "))
+        file_handle.write('\n')
+    # destroy streams
     file_handle.close()
     streamManagerApi.DestroyAllStreams()
