89c176e1d914ce26109ec9fce4b0ef775435ddef
蒋豪 <525567032@qq.com>
2021-08-18 06:33:09 +0000

    update contrib/human_segmentation/run.sh.

--- contrib/human_segmentation/run.sh
+++ contrib/human_segmentation/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Copyright(C) Huawei Technologies Co.,Ltd. 2012-2021 All rights reserved.
+# Copyright(C) 2021. Huawei Technologies Co.,Ltd. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,12 +14,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CUR_PATH=$(cd "$(dirname "$0")"; pwd)
+set -e
 
-echo $CUR_PATH
+# curr path
+cur_path=$(cd "$(dirname "$0")" || exit; pwd)
+# env ready
+env_ready=true
 
-export LD_LIBRARY_PATH="${MX_SDK_HOME}/lib":"${MX_SDK_HOME}/opensource/lib":"${MX_SDK_HOME}/opensource/lib64":"/usr/local/Ascend/ascend-toolkit/latest/acllib/lib64":"/usr/local/Ascend/driver/lib64/ ":${LD_LIBRARY_PATH}
-export GST_PLUGIN_SCANNER="${MX_SDK_HOME}/opensource/libexec/gstreamer-1.0/gst-plugin-scanner"
-export GST_PLUGIN_PATH="${MX_SDK_HOME}/opensource/lib/gstreamer-1.0":"${MX_SDK_HOME}/lib/plugins"
+function check_env() {
 
-./sample
+    # check MindXSDK env
+    if [ ! "${MX_SDK_HOME}" ]; then
+      env_ready=false
+      echo "please set MX_SDK_HOME path into env."
+    else
+      echo "MX_SDK_HOME set as ${MX_SDK_HOME}, ready."
+    fi
+}
+
+function export_env() {
+    export LD_LIBRARY_PATH=${MX_SDK_HOME}/lib:${MX_SDK_HOME}/opensource/lib:${MX_SDK_HOME}/opensource/lib64:${FFMPEG_HOME}/lib:/usr/local/Ascend/ascend-toolkit/latest/acllib/lib64:/usr/local/Ascend/driver/lib64/:${LD_LIBRARY_PATH}
+    export GST_PLUGIN_SCANNER=${MX_SDK_HOME}/opensource/libexec/gstreamer-1.0/gst-plugin-scanner
+    export GST_PLUGIN_PATH=${MX_SDK_HOME}/opensource/lib/gstreamer-1.0:${MX_SDK_HOME}/lib/plugins
+}
+
+echo "current dir: $cur_path"
+
+check_env
+if [ "${env_ready}" == false ]; then
+  echo "please set env first."
+  exit 0
+fi
+
+export_env
+echo "export env success."
+echo "prepare to execute main program."
+
+# check file
+if [ ! -f "${cur_path}/sample" ]; then
+  echo "sample not exist, please build first."
+else
+  # execute
+  ./sample
+fi
+
+exit 0
