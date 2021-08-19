bbe5d9d3e3eb64631064a53ab860fd55c48ac91c
wyz0916 <wyz_hbqhd_0916@163.com>
2021-08-19 13:24:45 +0000

    update contrib/Individual/main.py.

--- contrib/Individual/main.py
+++ contrib/Individual/main.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from StreamManagerApi import StreamManagerApi
+from StreamManagerApi import StreamManagerApi,MxDataInput
 
 if __name__ == '__main__':
     # init stream manager
