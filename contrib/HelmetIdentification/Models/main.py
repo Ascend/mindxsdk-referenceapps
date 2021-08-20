3b584683a8b99a7b8affd71a43f9128f87183d5b
刘凯 <805571035@qq.com>
2021-08-20 02:07:51 +0000

    update contrib/HelmetIdentification/Models/main.py.

--- contrib/HelmetIdentification/Models/main.py
+++ contrib/HelmetIdentification/Models/main.py
@@ -1,4 +1,3 @@
-"""
 # Copyright 2021 Huawei Technologies Co., Ltd
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +10,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
 
 import cv2
 import numpy as np
