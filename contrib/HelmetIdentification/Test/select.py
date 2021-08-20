960737f1ae757ed764434847868e1a0c0d415e9d
刘凯 <805571035@qq.com>
2021-08-20 02:09:49 +0000

    update contrib/HelmetIdentification/Test/select.py.

--- contrib/HelmetIdentification/Test/select.py
+++ contrib/HelmetIdentification/Test/select.py
@@ -1,4 +1,3 @@
-"""
 # Copyright 2021 Huawei Technologies Co., Ltd
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
+
 import os
 import shutil
 import cv2
