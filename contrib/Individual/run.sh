3fb4e476ec995547813f703aeb5735b84c1c4a22
wyz0916 <wyz_hbqhd_0916@163.com>
2021-08-25 08:46:18 +0000

    update contrib/Individual/run.sh.

--- contrib/Individual/run.sh
+++ contrib/Individual/run.sh
@@ -24,7 +24,7 @@ warn() { echo >&2 -e "\033[1;31m[WARN ][MxStream] $1\033[1;37m" ; }
 export MX_SDK_HOME=${CUR_PATH}/../..
 export LD_LIBRARY_PATH=${MX_SDK_HOME}/lib:${MX_SDK_HOME}/opensource/lib:${MX_SDK_HOME}/opensource/lib64:/usr/local/Ascend/ascend-toolkit/latest/acllib/lib64:${LD_LIBRARY_PATH}
 export GST_PLUGIN_SCANNER=${MX_SDK_HOME}/opensource/libexec/gstreamer-1.0/gst-plugin-scanner
-export GST_PLUGIN_PATH=${MX_SDK_HOME}/opensource/lib/gstreamer-1.0:${MX_SDK_HOME}/lib/plugins:/home/nku_wang6/mxVision-2.0.2/samples/mindx_sdk_plugin
+export GST_PLUGIN_PATH=${MX_SDK_HOME}/opensource/lib/gstreamer-1.0:${MX_SDK_HOME}/lib/plugins:${MX_SDK_HOME}/samples/mindx_sdk_plugin
 
 #to set PYTHONPATH, import the StreamManagerApi.py
 export PYTHONPATH=$PYTHONPATH:${MX_SDK_HOME}/python
