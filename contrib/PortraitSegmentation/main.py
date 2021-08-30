8b6900ca9a85d8374911a6455bf44a92c8ba6ae0
晏国志 <yanguozhiup@163.com>
2021-08-30 05:39:27 +0000

    update contrib/PortraitSegmentation/main.py.

--- contrib/PortraitSegmentation/main.py
+++ contrib/PortraitSegmentation/main.py
@@ -119,4 +119,4 @@ if __name__ == '__main__':
     maskExpansion = np.repeat(maskResize[..., np.newaxis], REPEAT_AXIS, REPEAT_TIMES)
     result = np.uint8(background * maskExpansion + portrait * (1 - maskExpansion))
     # save the new image in current catalog
-    cv2.imwrite('result/result.jpg', result) 
+    cv2.imwrite('result/result.jpg', result)
