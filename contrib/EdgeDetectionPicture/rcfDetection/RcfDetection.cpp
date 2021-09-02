17895cda1549dd79664216a787906b841cd8073b
yx_miao <yuxia@iscas.ac.cn>
2021-09-02 14:52:56 +0800

    check RcfDetection.cpp

--- contrib/EdgeDetectionPicture/rcfDetection/RcfDetection.cpp
+++ contrib/EdgeDetectionPicture/rcfDetection/RcfDetection.cpp
@@ -31,7 +31,7 @@ namespace{
 }
 
 void RcfDetection::SetRcfPostProcessConfig(const InitParam &initParam,
-                                                       std::map<std::string, std::shared_ptr<void>> &config) {
+                                           std::map<std::string, std::shared_ptr<void>> &config) {
     MxBase::ConfigData configData;
     const std::string checkTensor = initParam.checkTensor ? "true" : "false";
     
@@ -94,7 +94,7 @@ APP_ERROR RcfDetection::DeInit() {
 }
 
 APP_ERROR RcfDetection::ReadImage(const std::string &imgPath,
-	       	MxBase::TensorBase &tensor) {
+	                          MxBase::TensorBase &tensor) {
     MxBase::DvppDataInfo output = {};
     APP_ERROR ret = dvppWrapper_->DvppJpegDecode(imgPath, output);
     if (ret != APP_ERR_OK) {
@@ -102,21 +102,20 @@ APP_ERROR RcfDetection::ReadImage(const std::string &imgPath,
         return ret;
     }
     MxBase::MemoryData memoryData((void*)output.data, output.dataSize, 
-	                            MxBase::MemoryData::MemoryType::MEMORY_DEVICE, deviceId_);
+		                  MxBase::MemoryData::MemoryType::MEMORY_DEVICE, deviceId_);
     if (output.heightStride % VPC_H_ALIGN != 0) {
         LogError << "Output data height(" << output.heightStride << ") can't be divided by " << VPC_H_ALIGN << ".";
         MxBase::MemoryHelper::MxbsFree(memoryData);
         return APP_ERR_COMM_INVALID_PARAM;
     }
     dvppHeightStride = output.heightStride;
-    dvppWidthStride = output.widthStride;    
+    dvppWidthStride = output.widthStride;
     std::vector<uint32_t> shape = {output.heightStride * YUV_BYTE_NU/YUV_BYTE_DE , output.widthStride};
     tensor = MxBase::TensorBase(memoryData, false, shape, MxBase::TENSOR_DTYPE_UINT8);
     return APP_ERR_OK;
 }
-
-APP_ERROR RcfDetection::Resize(const MxBase::TensorBase &inputTensor, MxBase::TensorBase &outputTensor, 
-		               uint32_t resizeHeight, uint32_t resizeWidth) {    
+APP_ERROR RcfDetection::Resize(const MxBase::TensorBase &inputTensor, MxBase::TensorBase &outputTensor,
+	                       uint32_t resizeHeight, uint32_t resizeWidth) {
     auto shape = inputTensor.GetShape();
     MxBase::DvppDataInfo input = {};
     input.height = (uint32_t)shape[0] * YUV_BYTE_DE / YUV_BYTE_NU;
@@ -134,8 +133,8 @@ APP_ERROR RcfDetection::Resize(const MxBase::TensorBase &inputTensor, MxBase::Te
         LogError << "VpcResize failed, ret=" << ret << ".";
         return ret;
     }
-    MxBase::MemoryData memoryData((void*)output.data, output.dataSize, 
-	                            MxBase::MemoryData::MemoryType::MEMORY_DEVICE, deviceId_);
+    MxBase::MemoryData memoryData((void*)output.data, output.dataSize,
+		                 MxBase::MemoryData::MemoryType::MEMORY_DEVICE, deviceId_);
     if (output.heightStride % VPC_H_ALIGN != 0) {
         LogError << "Output data height(" << output.heightStride << ") can't be divided by " << VPC_H_ALIGN << ".";
         MxBase::MemoryHelper::MxbsFree(memoryData);
@@ -178,8 +177,8 @@ APP_ERROR RcfDetection::Inference(const std::vector<MxBase::TensorBase> &inputs,
 }
 
 APP_ERROR RcfDetection::PostProcess(const MxBase::TensorBase &tensor,
-	                           const std::vector<MxBase::TensorBase> &outputs,
-		                   std::vector<MxBase::TensorBase> &postProcessOutput) {    
+		                                const std::vector<MxBase::TensorBase> &outputs,
+						                        std::vector<MxBase::TensorBase> &postProcessOutput) {
     auto shape = tensor.GetShape();
     MxBase::ResizedImageInfo imgInfo;
     imgInfo.widthOriginal = shape[1];
@@ -202,27 +201,27 @@ APP_ERROR RcfDetection::PostProcess(const MxBase::TensorBase &tensor,
     return APP_ERR_OK;
 }
 
-APP_ERROR RcfDetection::WriteResult(MxBase::TensorBase &inferTensor,const std::string &imgPath) {    
+APP_ERROR RcfDetection::WriteResult(MxBase::TensorBase &inferTensor,const std::string &imgPath) {
     auto shape = inferTensor.GetShape();
     uint32_t height = shape[2];
     uint32_t width = shape[3];
     cv::Mat imgBgr = cv::imread(imgPath);
     uint32_t imageWidth = imgBgr.cols;
-    uint32_t imageHeight = imgBgr.rows;    
+    uint32_t imageHeight = imgBgr.rows;
     cv::Mat modelOutput = cv::Mat(height, width, CV_32FC1, inferTensor.GetBuffer());
     cv::Mat grayMat;
     cv::Mat resizedMat;
     int crop = 5;
-    cv::Rect myROI(0, 0, imageWidth-crop, imageHeight );	
+    cv::Rect myROI(0, 0, imageWidth-crop, imageHeight );
     resize(modelOutput, resizedMat, cv::Size(dvppWidthStride, dvppHeightStride), 0, 0, cv::INTER_LINEAR);
     resizedMat.convertTo(grayMat, CV_8UC1, alpha1);
     cv::Mat croppedImage = grayMat(myROI);
     resize(croppedImage, croppedImage,  cv::Size(imageWidth, imageHeight), 0, 0, cv::INTER_LINEAR);
     cv::imwrite("./result.jpg", croppedImage);
-    return APP_ERR_OK; 
+    return APP_ERR_OK;
 }
 
-APP_ERROR RcfDetection::Process(const std::string &imgPath) { 
+APP_ERROR RcfDetection::Process(const std::string &imgPath) {
     MxBase::TensorBase inTensor;
     APP_ERROR ret = ReadImage(imgPath, inTensor);
     if (ret != APP_ERR_OK) {
