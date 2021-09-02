90201f6ff3546c5ef84d54ddad98ca65477c6711
刘凯 <805571035@qq.com>
2021-09-02 01:36:15 +0000

    update contrib/HelmetIdentification/Models/main.py.

--- contrib/HelmetIdentification/Models/main.py
+++ contrib/HelmetIdentification/Models/main.py
@@ -12,13 +12,29 @@
 # limitations under the License.
 
 import cv2
+import signal
 import numpy as np
-import random
-import os
-import time
 import StreamManagerApi
 import MxpiDataType_pb2 as MxpiDataType
+import utils
 
+
+def my_handler(signum, frame):
+    """
+    :param signum: signum are used to identify the signal
+    :param frame: When the signal occurs, get the status of the process stack
+    func:Change flag of stop_stream
+    """
+    global stop_stream
+    stop_stream = True
+
+
+# exit flag
+stop_stream = False
+# When about to exit, get the exit signal
+signal.signal(signal.SIGINT, my_handler)
+
+# The following belongs to the SDK Process
 # init stream manager
 streamManagerApi = StreamManagerApi.StreamManagerApi()
 ret = streamManagerApi.InitManager()
@@ -35,7 +51,7 @@ if ret != 0:
     print("Failed to create Stream, ret=%s" % str(ret))
 
 # Obtain the inference result by specifying streamName and keyVec
-# The data that needs to be obtained is searched by the plugin name
+# The data that needs to be obtained is searched by the plug-in name
 # Stream name
 streamName = b'Detection'
 keyVec0 = StreamManagerApi.StringVector()
@@ -44,13 +60,11 @@ keyVec0.push_back(b"mxpi_modelinfer0")
 keyVec0.push_back(b"mxpi_motsimplesort0")
 keyVec0.push_back(b"mxpi_videodecoder0")
 keyVec0.push_back(b"mxpi_videodecoder1")
-# Output data required by the second output
-keyVec1 = StreamManagerApi.StringVector()
-keyVec1.push_back(b"ReservedFrameInfo")
-keyVec1.push_back(b"mxpi_videodecoder0")
-keyVec1.push_back(b"mxpi_videodecoder1")
-i = 0
+
 while True:
+    # exit flag
+    if stop_stream:
+        break
     # Get data through GetProtobuf interface
     inferResult0 = streamManagerApi.GetProtobuf(streamName, 0, keyVec0)
     # output errorCode
@@ -60,138 +74,10 @@ while True:
             print('Object detection result of model infer is null!!!')
         continue
 
-    # add inferennce data into DATA structure
-    # Frame information structure
-    FrameList0 = MxpiDataType.MxpiFrameInfo()
-    FrameList0.ParseFromString(inferResult0[0].messageBuf)
-    # Target object structure
-    ObjectList = MxpiDataType.MxpiObjectList()
-    ObjectList.ParseFromString(inferResult0[1].messageBuf)
-    # Get target box information
-    ObjectListData = ObjectList.objectVec
-    # track structure
-    trackLetList = MxpiDataType.MxpiTrackLetList()
-    trackLetList.ParseFromString(inferResult0[2].messageBuf)
-    # Obtain tracking information
-    trackLetData = trackLetList.trackLetVec
-    # image structure
-    visionList0 = MxpiDataType.MxpiVisionList()
-    visionList0.ParseFromString(inferResult0[3].messageBuf)
-    visionData0 = visionList0.visionVec[0].visionData.dataStr
-    # Get picture information
-    visionInfo0 = visionList0.visionVec[0].visionInfo
-
-    # cv2:YUV2BGR
-    YUV_BYTES_NU = 3
-    YUV_BYTES_DE = 2
-    img_yuv = np.frombuffer(visionData0, dtype=np.uint8)
-    # reshape
-    img_yuv = img_yuv.reshape(visionInfo0.heightAligned * YUV_BYTES_NU // YUV_BYTES_DE, visionInfo0.widthAligned)
-    # Color gamut conversion
-    img0 = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR_NV12)
-    t1 = time.time()
-    # put inference into dict,
-    imgLi1 = []
-    imgLi2 = []
-    for k in range(len(ObjectList.objectVec)):
-        imgLi = [round(ObjectListData[k].x0, 4), round(ObjectListData[k].x1, 4), round(ObjectListData[k].y0, 4),
-                 round(ObjectListData[k].y1, 4),
-                 round(ObjectListData[k].classVec[0].confidence, 4), ObjectListData[k].classVec[0].className,
-                 trackLetData[k].trackId, trackLetData[k].age]
-        imgLi1.append(imgLi)
-
-    # img1_shape is the size after padding
-    # img0_shape is the original image size
-    img1_shape = [640, 640]
-    img0_shape = [visionInfo0.heightAligned, visionInfo0.widthAligned]
-    print(img0_shape)
-    bboxes = []
-    # The color and title of the rectangle
-    color = [random.randint(0, 255) for _ in range(3)]
-    tl = (round(0.0002 * (img0.shape[0] + img0.shape[1])) + 0.35)
-    tf = max(tl - 1, 1)
-    # Add the inference results of head to imgLi2
-    for bbox0 in imgLi1:
-        if bbox0[5] == 'head':
-            imgLi2.append(bbox0)
-    for bbox1 in imgLi2:
-    # Determine whether it is helmet
-        bboxes = {'x0': int(bbox1[0]),
-                    'x1': int(bbox1[1]),
-                    'y0': int(bbox1[2]),
-                    'y1': int(bbox1[3]),
-                    'confidence': round(bbox1[4], 4),
-                    'trackid': int(bbox1[6]),
-                    'age': int(bbox1[7])
-                    }
-        print(bboxes)
-        L1 = []
-        L1.append(int(bboxes['x0']))
-        L1.append(int(bboxes['x1']))
-        L1.append(int(bboxes['y0']))
-        L1.append(int(bboxes['y1']))
-        L1 = np.array(L1, dtype=np.int32)
-        # Draw rectangle
-        cv2.putText(img0, str(bboxes['confidence']), (L1[0], L1[2]), 0, tl, [225, 255, 255], thickness=tf,
-                    lineType=cv2.LINE_AA)
-        # rectangle color [255,255,255]
-        cv2.rectangle(img0, (L1[0], L1[2]), (L1[1], L1[3]), (0, 0, 255), 2)
-        if bboxes['trackid'] is not None and bboxes['age'] == 1:
-            print("Warning:Not wearing a helmet,FrameId:{},InferenceId:{}".format(FrameList0.frameId,bboxes['trackid']))
-
-    # Save pictures in two ways
-    if FrameList0.channelId == 0:
-        oringe_imgfile = './output/one/image' + str(FrameList0.channelId) + '-' + str(
-            FrameList0.frameId) + '.jpg'
-        # Warning result save path
-        cv2.imwrite(oringe_imgfile, img0)
-    else:
-        # when channelId equal 1
-        oringe_imgfile = './output/two/image' + str(FrameList0.channelId) + '-' + str(
-            FrameList0.frameId) + '.jpg'
-        cv2.imwrite(oringe_imgfile, img0)
-
-    # output 6 frame info per inference
-    for i in range(6):
-        inferResult1 = streamManagerApi.GetProtobuf(streamName, 1, keyVec1)
-        if inferResult1[0].errorCode != 0:
-            print("GetProtobuf error. errorCode0=%d" % (inferResult1[0].errorCode))
-            break
-
-        # output inference data:frame info and img info
-        # Frame information structure
-        FrameList1 = MxpiDataType.MxpiFrameInfo()
-        # Get channel id and frame id
-        FrameList1.ParseFromString(inferResult1[0].messageBuf)
-        # image structure
-        visionList1 = MxpiDataType.MxpiVisionList()
-        visionList1.ParseFromString(inferResult1[1].messageBuf)
-        visionData1 = visionList1.visionVec[0].visionData.dataStr
-        # Get picture information
-        visionInfo1 = visionList1.visionVec[0].visionInfo
-
-        # cv2:YUV2BGR
-        YUV_BYTES_NU = 3
-        YUV_BYTES_DE = 2
-        img_yuv = np.frombuffer(visionData1, dtype=np.uint8)
-        # reshape
-        img_yuv = img_yuv.reshape(visionInfo1.heightAligned * YUV_BYTES_NU // YUV_BYTES_DE, visionInfo1.widthAligned)
-        # Color gamut conversion
-        img = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR_NV12)
-        # Save pictures in two ways
-        if FrameList1.channelId == 0:
-            oringe_imgfile = './output/one/image' + str(FrameList1.channelId) + '-' + str(
-                FrameList1.frameId) + '.jpg'
-            if os.path.exists(oringe_imgfile):
-                os.remove(oringe_imgfile)
-            cv2.imwrite(oringe_imgfile, img)
-        else:
-            # when channelId equal 1
-            oringe_imgfile = './output/two/image' + str(FrameList1.channelId) + '-' + str(
-                FrameList1.frameId) + '.jpg'
-            if os.path.exists(oringe_imgfile):
-                os.remove(oringe_imgfile)
-            cv2.imwrite(oringe_imgfile, img)
+    # Take the inference result from the corresponding data structure
+    img0, imgLi1, img0_shape, frameId, channelId = utils.get_inference_data(inferResult0)
+    # the visualization of the inference result, save the output in the specified folder
+    utils.Visualization(img0, imgLi1, img0_shape, frameId, channelId)
 
 # Destroy All Streams
 streamManagerApi.DestroyAllStreams()
