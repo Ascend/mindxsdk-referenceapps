f2c42137c76be049daf31d6abb312fc3ace13d47
yx_miao <yuxia@iscas.ac.cn>
2021-09-02 14:57:28 +0800

    check RcfDetection.cpp

--- contrib/EdgeDetectionPicture/rcfDetection/RcfDetection.cpp
+++ contrib/EdgeDetectionPicture/rcfDetection/RcfDetection.cpp
@@ -12,10 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- **/
-
-#include "opencv2/opencv.hpp"
+ */
 #include "RcfDetection.h"
+#include "opencv2/opencv.hpp"
 #include "MxBase/DeviceManager/DeviceManager.h"
 #include "MxBase/Log/Log.h"
 #include "../rcfPostProcess/RcfPostProcess.h"
