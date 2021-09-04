664a6e7410d463b091902db19396426375da3276
huangheyang <352005348@qq.com>
2021-09-04 12:00:02 +0800

    门禁的修改

--- tutorials/DvppWrapperSample/main.cpp
+++ tutorials/DvppWrapperSample/main.cpp
@@ -21,7 +21,6 @@
 using namespace std;
 
 namespace {
-    typedef char * char_str;
     using namespace MxBase;
 
     const uint32_t ENCODE_TEST_DEVICE_ID = 1;
@@ -181,7 +180,7 @@ namespace {
             LogError << "Failed to copy data to host";
             return ret;
         }
-        std::string result(static_cast<char_str>(des.ptrData), des.size);
+        std::string result(static_cast<char*>(des.ptrData), des.size);
         std::string content = ReadFileContent("./decode.jpg");
         if (result == content) {
             LogInfo << "Decode success";
