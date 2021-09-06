75f0194a40ade8e38cec891e864da51451e9111e
刘凯 <805571035@qq.com>
2021-09-06 08:52:12 +0000

    update contrib/HelmetIdentification/Models/utils.py.

--- contrib/HelmetIdentification/Models/utils.py
+++ contrib/HelmetIdentification/Models/utils.py
@@ -3,25 +3,25 @@ import cv2
 import numpy as np
 import MxpiDataType_pb2 as MxpiDataType
 
-def cv_visualization(img, infer, shape, frameId, channelId):
+def cv_visualization(img, infer, shape, frame_id, channel_id):
     """
     :param img: Inference image
     :param infer: Inference result
     :param shape:Size before image padding
-    :param frameId:Inference image id
-    :param channelId:Channel id of the current inference image
+    :param frame_id:Inference image id
+    :param channel_id:Channel id of the current inference image
     func: the visualization of the inference result, save the output in the specified folder
     """
-    imgLi2 = []
+    img_list2 = []
     print(shape)
     # The title of the rectangle
-    tl = (round(0.0002 * (shape[0] + shape[1])) + 0.35)
-    tf = max(tl - 1, 1)
-    # Add the inference results of head to imgLi2
+    title_l = (round(0.0002 * (shape[0] + shape[1])) + 0.35)
+    title_f = max(title_l - 1, 1)
+    # Add the inference results of head to img_list2
     for bbox0 in infer:
         if bbox0[5] == 'head':
-            imgLi2.append(bbox0)
-    for bbox1 in imgLi2:
+            img_list2.append(bbox0)
+    for bbox1 in img_list2:
         # Determine whether it is helmet
         bboxes = {'x0': int(bbox1[0]),
                   'x1': int(bbox1[1]),
@@ -32,79 +32,79 @@ def cv_visualization(img, infer, shape, frameId, channelId):
                   'age': int(bbox1[7])
                   }
         print(bboxes)
-        L1 = []
-        L1.append(int(bboxes['x0']))
-        L1.append(int(bboxes['x1']))
-        L1.append(int(bboxes['y0']))
-        L1.append(int(bboxes['y1']))
-        L1 = np.array(L1, dtype=np.int32)
+        bboxes_list1 = []
+        bboxes_list1.append(int(bboxes['x0']))
+        bboxes_list1.append(int(bboxes['x1']))
+        bboxes_list1.append(int(bboxes['y0']))
+        bboxes_list1.append(int(bboxes['y1']))
+        bboxes_list1 = np.array(bboxes_list1, dtype=np.int32)
         # Draw rectangle
-        cv2.putText(img, str(bboxes['confidence']), (L1[0], L1[2]), 0, tl, [225, 255, 255], thickness=tf,
+        cv2.putText(img, str(bboxes['confidence']), (bboxes_list1[0], bboxes_list1[2]), 0, title_l, [225, 255, 255], thickness=title_f,
                     lineType=cv2.LINE_AA)
         # rectangle color [255,255,255]
-        cv2.rectangle(img, (L1[0], L1[2]), (L1[1], L1[3]), (0, 0, 255), 2)
+        cv2.rectangle(img, (bboxes_list1[0], bboxes_list1[2]), (bboxes_list1[1], bboxes_list1[3]), (0, 0, 255), 2)
         if bboxes['trackid'] is not None and bboxes['age'] == 1:
-            print("Warning:Not wearing a helmet,InferenceId:{},FrameId:{}".format(bboxes['trackid'], frameId))
+            print("Warning:Not wearing a helmet,InferenceId:{},FrameId:{}".format(bboxes['trackid'], frame_id))
 
     # Save pictures in two ways
-    if channelId == 0:
-        oringe_imgfile = './output/one/image' + str(channelId) + '-' + str(
-            frameId) + '.jpg'
+    if channel_id == 0:
+        oringe_imgfile = './output/one/image' + str(channel_id) + '-' + str(
+            frame_id) + '.jpg'
         # Warning result save path
         cv2.imwrite(oringe_imgfile, img)
     else:
-        # when channelId equal 1
-        oringe_imgfile = './output/two/image' + str(channelId) + '-' + str(
-            frameId) + '.jpg'
+        # when channel_id equal 1
+        oringe_imgfile = './output/two/image' + str(channel_id) + '-' + str(
+            frame_id) + '.jpg'
         cv2.imwrite(oringe_imgfile, img)
 
 
 def get_inference_data(inference):
     """
     :param inference:output of sdk stream inference
-    :return:img0, imgLi1, img0_shape, FrameList0.frameId, FrameList0.channelId
+    :return:img0, img_list1, img0_shape, Frame_List0.frameId, Frame_List0.channelId
     """
 
     # add inferennce data into DATA structure
     # Frame information structure
-    FrameList0 = MxpiDataType.MxpiFrameInfo()
-    FrameList0.ParseFromString(inference[0].messageBuf)
+    Frame_List0 = MxpiDataType.MxpiFrameInfo()
+    Frame_List0.ParseFromString(inference[0].messageBuf)
     # Target object structure
-    ObjectList = MxpiDataType.MxpiObjectList()
-    ObjectList.ParseFromString(inference[1].messageBuf)
+    object_list = MxpiDataType.MxpiObjectList()
+    object_list.ParseFromString(inference[1].messageBuf)
     # Get target box information
-    ObjectListData = ObjectList.objectVec
+    objectlist_data = object_list.objectVec
     # track structure
-    trackLetList = MxpiDataType.MxpiTrackLetList()
-    trackLetList.ParseFromString(inference[2].messageBuf)
+    trackLet_list = MxpiDataType.MxpiTrackLetList()
+    trackLet_list.ParseFromString(inference[2].messageBuf)
     # Obtain tracking information
-    trackLetData = trackLetList.trackLetVec
+    trackLet_data = trackLet_list.trackLetVec
     # image structure
-    visionList0 = MxpiDataType.MxpiVisionList()
-    visionList0.ParseFromString(inference[3].messageBuf)
-    visionData0 = visionList0.visionVec[0].visionData.dataStr
+    vision_list0 = MxpiDataType.MxpiVisionList()
+    vision_list0.ParseFromString(inference[3].messageBuf)
+    vision_data0 = vision_list0.visionVec[0].visionData.dataStr
     # Get picture information
-    visionInfo0 = visionList0.visionVec[0].visionInfo
+    vision_info0 = vision_list0.visionVec[0].visionInfo
 
     # cv2 func YUV to BGR
-    YUV_BYTES_NU = 3
-    YUV_BYTES_DE = 2
-    img_yuv = np.frombuffer(visionData0, dtype=np.uint8)
+    yuv_bytes_nu = 3
+    yuv_bytes_de = 2
+    img_yuv = np.frombuffer(vision_data0, dtype=np.uint8)
     # reshape
-    img_yuv = img_yuv.reshape(visionInfo0.heightAligned * YUV_BYTES_NU // YUV_BYTES_DE, visionInfo0.widthAligned)
+    img_yuv = img_yuv.reshape(vision_info0.heightAligned * yuv_bytes_nu // yuv_bytes_de, vision_info0.widthAligned)
     # Color gamut conversion
     img0 = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR_NV12)
     # put inference into dict,
-    imgLi1 = []
-    for k in range(len(ObjectList.objectVec)):
-        imgLi = [round(ObjectListData[k].x0, 4), round(ObjectListData[k].x1, 4), round(ObjectListData[k].y0, 4),
-                 round(ObjectListData[k].y1, 4),
-                 round(ObjectListData[k].classVec[0].confidence, 4), ObjectListData[k].classVec[0].className,
-                 trackLetData[k].trackId, trackLetData[k].age]
-        imgLi1.append(imgLi)
+    img_list1 = []
+    for k in range(len(object_list.objectVec)):
+        img_list = [round(objectlist_data[k].x0, 4), round(objectlist_data[k].x1, 4), round(objectlist_data[k].y0, 4),
+                 round(objectlist_data[k].y1, 4),
+                 round(objectlist_data[k].classVec[0].confidence, 4), objectlist_data[k].classVec[0].className,
+                 trackLet_data[k].trackId, trackLet_data[k].age]
+        img_list1.append(img_list)
 
     # img0_shape is the original image size
-    img0_shape = [visionInfo0.heightAligned, visionInfo0.widthAligned]
+    img0_shape = [vision_info0.heightAligned, vision_info0.widthAligned]
     # Output the results uniformly through the dictionary
-    DictStructure = [img0, imgLi1, img0_shape, FrameList0.frameId, FrameList0.channelId]
-    return DictStructure
+    dict_structure = [img0, img_list1, img0_shape, Frame_List0.frameId, Frame_List0.channelId]
+    return dict_structure
