ee49fb5334aeb681457666262fb47eb957c3ada8
王胤哲 <wyz_hbqhd_0916@163.com>
2021-09-06 07:14:39 +0000

    update contrib/Individual/cal_accuracy.py.

--- contrib/Individual/cal_accuracy.py
+++ contrib/Individual/cal_accuracy.py
@@ -15,8 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import numpy as np
 import argparse
+import numpy as np
 
 # define the arguments
 def get_arguments():
