fc5b6493f008928ad2ab5cb39b211e68641da996
刘凯 <805571035@qq.com>
2021-09-14 07:00:09 +0000

    update contrib/HelmetIdentification/Models/main.py.

--- contrib/HelmetIdentification/Models/main.py
+++ contrib/HelmetIdentification/Models/main.py
@@ -11,8 +11,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import signal
 import cv2
+import time
+import signal
 import numpy as np
 import StreamManagerApi
 import utils
@@ -22,61 +23,63 @@ def my_handler(signum, frame):
     """
     :param signum: signum are used to identify the signal
     :param frame: When the signal occurs, get the status of the process stack
-    func:Change flag of STOP_STREAM
+    func:Change flag of stop_stream
     """
-    global STOP_STREAM
-    STOP_STREAM = True
+    global stop_stream
+    stop_stream = True
 
 
 # exit flag
-STOP_STREAM = False
+stop_stream = False
 # When about to exit, get the exit signal
 signal.signal(signal.SIGINT, my_handler)
 
 # The following belongs to the SDK Process
 # init stream manager
-STREAMMANAGERAPI = StreamManagerApi.StreamManagerApi()
-RET = STREAMMANAGERAPI.InitManager()
-if RET != 0:
-    print("Failed to init Stream manager, ret=%s" % str(RET))
+streamManagerApi = StreamManagerApi.StreamManagerApi()
+ret = streamManagerApi.InitManager()
+if ret != 0:
+    print("Failed to init Stream manager, ret=%s" % str(ret))
 
 # create streams by pipeline config file
 #load  pipline
 with open("HelmetDetection.pipline", 'rb') as f:
-    PIPELINESTR = f.read()
-RET = STREAMMANAGERAPI.CreateMultipleStreams(PIPELINESTR)
+    pipelineStr = f.read()
+ret = streamManagerApi.CreateMultipleStreams(pipelineStr)
 # Print error message
-if RET != 0:
-    print("Failed to create Stream, ret=%s" % str(RET))
+if ret != 0:
+    print("Failed to create Stream, ret=%s" % str(ret))
 
 # Obtain the inference result by specifying streamName and keyVec
 # The data that needs to be obtained is searched by the plug-in name
 # Stream name
-STREAMNAME = b'Detection'
-KEYVEC0 = STREAMMANAGERAPI.StringVector()
-KEYVEC0.push_back(b"ReservedFrameInfo")
-KEYVEC0.push_back(b"mxpi_modelinfer0")
-KEYVEC0.push_back(b"mxpi_motsimplesort0")
-KEYVEC0.push_back(b"mxpi_videodecoder0")
-KEYVEC0.push_back(b"mxpi_videodecoder1")
+streamName = b'Detection'
+keyVec0 = StreamManagerApi.StringVector()
+keyVec0.push_back(b"ReservedFrameInfo")
+keyVec0.push_back(b"mxpi_modelinfer0")
+keyVec0.push_back(b"mxpi_motsimplesort0")
+keyVec0.push_back(b"mxpi_videodecoder0")
+keyVec0.push_back(b"mxpi_videodecoder1")
 
 while True:
     # exit flag
-    if STOP_STREAM:
+    if stop_stream:
         break
     # Get data through GetProtobuf interface
-    INFERRESULT0 = STREAMMANAGERAPI.GetProtobuf(STREAMNAME, 0, KEYVEC0)
+    inferResult0 = streamManagerApi.GetProtobuf(streamName, 0, keyVec0)
     # output errorCode
-    if INFERRESULT0[0].errorCode != 0:
+    if inferResult0[0].errorCode != 0:
         # Print error message
-        if INFERRESULT0[0].errorCode == 1001:
+        if inferResult0[0].errorCode == 1001:
             print('Object detection result of model infer is null!!!')
         continue
+    if inferResult0 is None:
+        print("Warning:Input information not detected")
+        break
 
-    # Take the inference result from the corresponding data structure
-    DICTSTRUCTURE = utils.get_inference_data(INFERRESULT0)
+    DictStructure = utils.get_inference_data(inferResult0)
     # the visualization of the inference result, save the output in the specified folder
-    utils.cv_visualization(DICTSTRUCTURE[0], DICTSTRUCTURE[1], DICTSTRUCTURE[2], DICTSTRUCTURE[3], DICTSTRUCTURE[4])
+    utils.Visualization(DictStructure[0], DictStructure[1], DictStructure[2], DictStructure[3], DictStructure[4])
 
 # Destroy All Streams
-STREAMMANAGERAPI.DestroyAllStreams()
+streamManagerApi.DestroyAllStreams()
