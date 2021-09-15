1e82610a063e25e22d77573fb46a84cdd41bf237
“刘新慧” <yushinhye@163.com>
2021-09-15 20:41:23 +0800

    test2

---
+++ contrib/FatigueDrivingRecognition/parallel_update.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Copyright(C) Huawei Technologies Co.,Ltd. 2012-2021 All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import cv2
+import numpy as np
+import threading
+import os
+import MxpiDataType_pb2 as MxpiDataType
+from StreamManagerApi import StreamManagerApi, MxDataInput, StringVector
+import time
+
+index = 0
+index_second = 0
+
+def fun_timer():
+    print("frame_mine:",index+index_second)
+    print("average:",(index+index_second)/60)
+    
+
+if __name__ == '__main__':
+    streamManagerApi = StreamManagerApi()
+    # init stream manager
+    ret = streamManagerApi.InitManager()
+    if ret != 0:
+        print("Failed to init Stream manager, ret=%s" % str(ret))
+        exit()
+
+    # create streams by pipeline config file
+    pipeline_path = b"parallel2.pipeline"
+    ret = streamManagerApi.CreateMultipleStreamsFromFile(pipeline_path)
+    if ret != 0:
+        print("Failed to create Stream, ret=%s" % str(ret))
+        exit()
+    
+    streamName = b"detection"
+    
+    keyVec = StringVector()
+    keyVec.push_back(b"ReservedFrameInfo")
+    keyVec.push_back(b"mxpi_tensorinfer1")
+    keyVec.push_back(b"mxpi_videodecoder0")
+    keyVec.push_back(b"mxpi_distributor0_0")
+    keyVec.push_back(b"mxpi_pfldpostprocess0")
+    keyVec.push_back(b"mxpi_videodecoder1")
+
+
+    img_yuv_list = []
+    heightAligned_list = []
+    widthAligned_list = []
+    isFatigue = 0
+    img_yuv_list_1 = []
+    isFatigue_1 = 0
+    YUV_BYTES_NU = 3
+    YUV_BYTES_DE = 2
+    img_yuv_list_2 = []
+    heightAligned_list_2 = []
+    widthAligned_list_2 = []
+    frame_num_list = [2741,2741]
+    MARS_1 = []
+    MARS_2 = []
+    
+    while True:
+        if index == frame_num_list[0] and index_second == frame_num_list[1]:
+            break
+        
+        if index == 0 and index_second == 0 :
+            timer = threading.Timer(60, fun_timer)
+            timer.start()
+        
+        infer_result = streamManagerApi.GetProtobuf(streamName, 0, keyVec)
+        tensorList = MxpiDataType.MxpiTensorPackageList()
+        tensorList.ParseFromString(infer_result[1].messageBuf)
+        ids = np.frombuffer(tensorList.tensorPackageVec[0].tensorVec[0].dataStr, dtype=np.float32)
+        if ids.shape[0] == 0:
+            continue
+        FrameList0 = MxpiDataType.MxpiFrameInfo()
+        FrameList0.ParseFromString(infer_result[0].messageBuf)
+        if index == frame_num_list[0] and FrameList0.channelId == 0:
+            continue
+        elif index_second == frame_num_list[1] and FrameList0.channelId == 1:
+            continue
+
+        if FrameList0.channelId == 0:
+            objectList = MxpiDataType.MxpiObjectList()
+            objectList.ParseFromString(infer_result[4].messageBuf)
+            MAR = objectList.objectVec[0].x0
+            height_left = objectList.objectVec[0].y0
+            height_right = objectList.objectVec[0].y1
+
+            visionList = MxpiDataType.MxpiVisionList()
+            visionList.ParseFromString(infer_result[2].messageBuf)
+            visionData = visionList.visionVec[0].visionData.dataStr
+            visionInfo = visionList.visionVec[0].visionInfo
+            
+
+            img_yuv = np.frombuffer(visionData, dtype=np.uint8)
+            heightAligned = visionInfo.heightAligned
+            widthAligned = visionInfo.widthAligned
+
+            MARS_1.append(MAR)
+            img_yuv_list.append(img_yuv)
+            heightAligned_list.append(heightAligned)
+            widthAligned_list.append(widthAligned)
+            if len(MARS_1) >= 30:
+                aim_MARS = MARS_1[-30:]
+                max_index = 0
+                max_mar = aim_MARS[0]
+                num = 0
+                for index_mar, mar in enumerate(aim_MARS):
+                    if mar >= 0.14:
+                        num += 1
+                    if mar > max_mar:
+                        max_mar = mar
+                        max_index = index_mar
+                
+                perclos = num / 30
+                
+                # threshold
+                if perclos >= 0.7:
+                    isFatigue = 1
+                    print('Fatigue!!!')
+
+                    img_yuv_fatigue = img_yuv_list[max_index]
+                    img_yuv_fatigue = img_yuv_fatigue.reshape(heightAligned_list[max_index] * YUV_BYTES_NU // YUV_BYTES_DE,
+                                                            widthAligned_list[max_index])
+                    img_fatigue = cv2.cvtColor(img_yuv_fatigue, cv2.COLOR_YUV2BGR_NV12)
+                    cv2.putText(img_fatigue, "Warning!!! Fatigue!!!", (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 2)
+                    index_print = index - 30 + max_index
+                    image_path = "fatigue/0/"
+                    if not os.path.exists(image_path):
+                        os.mkdir(image_path)
+                    image_name = image_path + str(index_print) + ".jpg"
+                    cv2.imwrite(image_name, img_fatigue)
+                heightAligned_list.pop(0)
+                widthAligned_list.pop(0)
+                img_yuv_list.pop(0)
+
+
+            index = index + 1
+
+        
+        elif FrameList0.channelId == 1:
+            objectList = MxpiDataType.MxpiObjectList()
+            objectList.ParseFromString(infer_result[3].messageBuf)
+            MAR = objectList.objectVec[0].x0
+            height_left = objectList.objectVec[0].y0
+            height_right = objectList.objectVec[0].y1
+
+            visionList = MxpiDataType.MxpiVisionList()
+            visionList.ParseFromString(infer_result[4].messageBuf)
+            visionData = visionList.visionVec[0].visionData.dataStr
+            visionInfo = visionList.visionVec[0].visionInfo
+            
+
+            img_yuv = np.frombuffer(visionData, dtype=np.uint8)
+            heightAligned = visionInfo.heightAligned
+            widthAligned = visionInfo.widthAligned
+
+            MARS_2.append(MAR)
+            img_yuv_list_2.append(img_yuv)
+            heightAligned_list_2.append(heightAligned)
+            widthAligned_list_2.append(widthAligned)
+            if len(MARS_2) >= 30:
+                aim_MARS = MARS_2[-30:]
+                max_index = 0
+                max_mar = aim_MARS[0]
+                num = 0
+                for index_mar, mar in enumerate(aim_MARS):
+                    if mar >= 0.14:
+                        num += 1
+                    if mar < max_mar:
+                        max_mar = mar
+                        max_index = index_mar
+                
+                perclos = num / 30
+                
+                # threshold
+                if perclos >= 0.7:
+                    isFatigue_1 = 1
+                    print('Fatigue!!!')
+                    img_yuv_fatigue = img_yuv_list_2[max_index]
+                    img_yuv_fatigue = img_yuv_fatigue.reshape(heightAligned_list_2[max_index] * YUV_BYTES_NU // YUV_BYTES_DE,
+                                                            widthAligned_list_2[max_index])
+                    img_fatigue = cv2.cvtColor(img_yuv_fatigue, cv2.COLOR_YUV2BGR_NV12)
+                    cv2.putText(img_fatigue, "Warning!!! Fatigue!!!", (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 2)
+                    index_print = index_second - 30 + max_index
+                    image_path = "fatigue/1/"
+                    if not os.path.exists(image_path):
+                        os.mkdir(image_path)
+                    image_name = image_path + str(index_print) + ".jpg"
+                    cv2.imwrite(image_name, img_fatigue)
+                heightAligned_list_2.pop(0)
+                widthAligned_list_2.pop(0)
+                img_yuv_list_2.pop(0)
+
+
+            index_second = index_second + 1
+
+    
+    if isFatigue == 0:
+        print('Normal')
+        
+    else:
+        print('Fatigue!!!')
+    if isFatigue_1 == 0:
+        print('1 is Normal')
+        
+    else:
+        print('1 is Fatigue!!!')
+
+    # destroy streams
+    streamManagerApi.DestroyAllStreams()
---
+++ contrib/FatigueDrivingRecognition/test_video.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Copyright(C) Huawei Technologies Co.,Ltd. 2012-2021 All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import cv2
+import numpy as np
+import os
+import time
+import threading
+import MxpiDataType_pb2 as MxpiDataType
+from StreamManagerApi import StreamManagerApi, MxDataInput, StringVector
+
+
+
+if __name__ == '__main__':
+    streamManagerApi = StreamManagerApi()
+    # init stream manager
+    ret = streamManagerApi.InitManager()
+    if ret != 0:
+        print("Failed to init Stream manager, ret=%s" % str(ret))
+        exit()
+
+    # create streams by pipeline config file
+    pipeline_path = b"test_video.pipeline"
+    ret = streamManagerApi.CreateMultipleStreamsFromFile(pipeline_path)
+    if ret != 0:
+        print("Failed to create Stream, ret=%s" % str(ret))
+        exit()
+    time_start=time.time()
+    streamName = b"detection"
+    
+    keyVec = StringVector()
+    keyVec.push_back(b"mxpi_tensorinfer1")
+    keyVec.push_back(b"mxpi_videodecoder0")
+    keyVec.push_back(b"mxpi_distributor0_0")
+    keyVec.push_back(b"mxpi_pfldpostprocess0")
+    
+    img_yuv_list = []
+    heightAligned_list = []
+    widthAligned_list = []
+    isFatigue = 0
+    YUV_BYTES_NU = 3
+    YUV_BYTES_DE = 2
+    MARS = []
+    index = 0
+    while True:
+        if index == 2741:
+            break
+        infer_result = streamManagerApi.GetProtobuf(streamName, 0, keyVec)
+
+        objectList = MxpiDataType.MxpiObjectList()
+        objectList.ParseFromString(infer_result[3].messageBuf)
+        MAR = objectList.objectVec[0].x0
+
+        visionList = MxpiDataType.MxpiVisionList()
+        visionList.ParseFromString(infer_result[1].messageBuf)
+        visionData = visionList.visionVec[0].visionData.dataStr
+        visionInfo = visionList.visionVec[0].visionInfo
+
+        img_yuv = np.frombuffer(visionData, dtype=np.uint8)
+        heightAligned = visionInfo.heightAligned
+        widthAligned = visionInfo.widthAligned
+        time_start_calcu = time.time()
+        MARS.append(MAR)
+        img_yuv_list.append(img_yuv)
+        heightAligned_list.append(heightAligned)
+        widthAligned_list.append(widthAligned)
+        # number of frame
+        if len(MARS) >= 30:
+            aim_MARS = MARS[-30:]
+            max_index = 0
+            max_mar = aim_MARS[0]
+            num = 0
+            for index_mar, mar in enumerate(aim_MARS):
+                if mar >= 0.14:
+                    num += 1
+                if mar > max_mar:
+                    max_mar = mar
+                    max_index = index_mar
+                
+            perclos = num / 30
+            # threshold
+            if perclos >= 0.7:
+                isFatigue = 1
+                print('Fatigue!!!')
+                img_yuv_fatigue = img_yuv_list[max_index]
+                img_yuv_fatigue = img_yuv_fatigue.reshape(heightAligned_list[max_index] * YUV_BYTES_NU // YUV_BYTES_DE,widthAligned_list[max_index])
+                img_fatigue = cv2.cvtColor(img_yuv_fatigue, cv2.COLOR_YUV2BGR_NV12)
+                cv2.putText(img_fatigue, "Warning!!! Fatigue!!!", (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 2)
+                index_print = index - 30 + max_index
+                image_path = "fatigue_1/1/"
+                if not os.path.exists(image_path):
+                    os.mkdir(image_path)
+                image_name = image_path + str(index_print) + ".jpg"
+                cv2.imwrite(image_name, img_fatigue)
+                
+        index = index + 1
+    
+    if isFatigue == 0:
+        print('Normal')
+    else:
+        print('Fatigue!!!')
+    # destroy streams
+    streamManagerApi.DestroyAllStreams()
