c0c1da6515e9c02ac1e063af65b0995889ab52d0
刘凯 <805571035@qq.com>
2021-09-22 13:52:36 +0000

    update contrib/HelmetIdentification/Models/main.py.

--- contrib/HelmetIdentification/Models/main.py
+++ contrib/HelmetIdentification/Models/main.py
@@ -11,9 +11,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import cv2
 import time
 import signal
+import cv2
 import numpy as np
 import StreamManagerApi
 import utils
