2907c66a5f2e15b440d76dc92b0edc4cab3a62f6
yingzhidaofei <396606075@qq.com>
2021-09-23 10:54:37 +0000

    update contrib/build_all.sh.

--- contrib/build_all.sh
+++ contrib/build_all.sh
@@ -18,6 +18,11 @@ current_folder="$( cd "$(dirname "$0")" ;pwd -P )"
 
 
 SAMPLE_FOLDER=(
+    ActionRecognition/
+	CrowdCounting/
+    HelmetIdentification/
+    Individual/
+    human_segmentation/
     mxBase_wheatDetection/
 )
 
