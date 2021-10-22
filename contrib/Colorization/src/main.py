7c89640dad71d7f3ec3ef26f534f74be18f9b45b
贾环宇 <383180957@qq.com>
2021-10-22 08:17:36 +0000

    update contrib/Colorization/src/main.py.

--- contrib/Colorization/src/main.py
+++ contrib/Colorization/src/main.py
@@ -1,4 +1,4 @@
-```
+'''
 #!/usr/bin/env python
 # coding=utf-8
 # Copyright(C) 2021. Huawei Technologies Co.,Ltd. All rights reserved.
@@ -14,7 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-```
+'''
 
 import sys
 import json
