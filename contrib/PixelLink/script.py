7c574577f971bc5f2838f1132de5963011236ac1
王胤哲 <wyz_hbqhd_0916@163.com>
2021-10-28 08:36:11 +0000

    update contrib/PixelLink/script.py.

--- contrib/PixelLink/script.py
+++ contrib/PixelLink/script.py
@@ -43,26 +43,26 @@ def default_evaluation_params():
                 'PER_SAMPLE_RESULTS': True #Generate per sample results and produce data for visualization
             }
 
-def validate_data(gtFilePath, submFilePath,evaluationParams):
+def validate_data(gtFilePath, submFilePath, evaluationParams):
     """
     Method validate_data: validates that all files in the results folder are correct (have the correct name contents).
                             Validates also that there are no missing files in the folder.
                             If some error detected, the method raises the error
     """
-    gt = rrc_evaluation_funcs.load_zip_file(gtFilePath,evaluationParams['GT_SAMPLE_NAME_2_ID'])
+    gt = rrc_evaluation_funcs.load_zip_file(gtFilePath, evaluationParams['GT_SAMPLE_NAME_2_ID'])
 
-    subm = rrc_evaluation_funcs.load_zip_file(submFilePath,evaluationParams['DET_SAMPLE_NAME_2_ID'], True)
+    subm = rrc_evaluation_funcs.load_zip_file(submFilePath, evaluationParams['DET_SAMPLE_NAME_2_ID'], True)
     
     #Validate format of GroundTruth
     for k in gt:
-        rrc_evaluation_funcs.validate_lines_in_file(k,gt[k],evaluationParams['CRLF'], evaluationParams['LTRB'], True)
+        rrc_evaluation_funcs.validate_lines_in_file(k, gt[k], evaluationParams['CRLF'], evaluationParams['LTRB'], True)
 
     #Validate format of results
     for k in subm:
-        if (k in gt) == False :
+        if (k in gt) == False:
             raise Exception("The sample %s not present in GT" % k)
         
-        rrc_evaluation_funcs.validate_lines_in_file(k,subm[k], evaluationParams['CRLF'], evaluationParams['LTRB'], False, evaluationParams['CONFIDENCES'])
+        rrc_evaluation_funcs.validate_lines_in_file(k, subm[k], evaluationParams['CRLF'], evaluationParams['LTRB'], False, evaluationParams['CONFIDENCES'])
 
     
 def evaluate_method(gtFilePath, submFilePath, evaluationParams):
@@ -80,45 +80,45 @@ def evaluate_method(gtFilePath, submFilePath, evaluationParams):
         """
         Returns a Polygon object to use with the Polygon2 class from a list of 8 points: x1,y1,x2,y2,x3,y3,x4,y4
         """        
-        resBoxes=np.empty([1,8],dtype = 'int32')
-        resBoxes[0,0] = int(points[0])
-        resBoxes[0,4] = int(points[1])
-        resBoxes[0,1] = int(points[2])
-        resBoxes[0,5] = int(points[3])
-        resBoxes[0,2] = int(points[4])
-        resBoxes[0,6] = int(points[5])
-        resBoxes[0,3] = int(points[6])
-        resBoxes[0,7] = int(points[7])
-        pointMat = resBoxes[0].reshape([2,4]).T
+        resBoxes=np.empty([1, 8], dtype = 'int32')
+        resBoxes[0, 0] = int(points[0])
+        resBoxes[0, 4] = int(points[1])
+        resBoxes[0, 1] = int(points[2])
+        resBoxes[0, 5] = int(points[3])
+        resBoxes[0, 2] = int(points[4])
+        resBoxes[0, 6] = int(points[5])
+        resBoxes[0, 3] = int(points[6])
+        resBoxes[0, 7] = int(points[7])
+        pointMat = resBoxes[0].reshape([2, 4]).T
         return plg.Polygon(pointMat)    
     
     def rectangle_to_polygon(rect):
         resBoxes=np.empty([1, 8], dtype = 'int32')
-        resBoxes[0,0] = int(rect.xmin)
-        resBoxes[0,4] = int(rect.ymax)
-        resBoxes[0,1] = int(rect.xmin)
-        resBoxes[0,5] = int(rect.ymin)
-        resBoxes[0,2] = int(rect.xmax)
-        resBoxes[0,6] = int(rect.ymin)
-        resBoxes[0,3] = int(rect.xmax)
-        resBoxes[0,7] = int(rect.ymax)
+        resBoxes[0, 0] = int(rect.xmin)
+        resBoxes[0, 4] = int(rect.ymax)
+        resBoxes[0, 1] = int(rect.xmin)
+        resBoxes[0, 5] = int(rect.ymin)
+        resBoxes[0, 2] = int(rect.xmax)
+        resBoxes[0, 6] = int(rect.ymin)
+        resBoxes[0, 3] = int(rect.xmax)
+        resBoxes[0, 7] = int(rect.ymax)
 
-        pointMat = resBoxes[0].reshape([2,4]).T
+        pointMat = resBoxes[0].reshape([2, 4]).T
         
-        return plg.Polygon( pointMat)
+        return plg.Polygon(pointMat)
     
     def rectangle_to_points(rect):
         points = [int(rect.xmin), int(rect.ymax), int(rect.xmax), int(rect.ymax), int(rect.xmax), int(rect.ymin), int(rect.xmin), int(rect.ymin)]
         return points
         
     def get_union(pD, pG):
-        areaA = pD.area();
-        areaB = pG.area();
-        return areaA + areaB - get_intersection(pD, pG);
+        areaA = pD.area()
+        areaB = pG.area()
+        return areaA + areaB - get_intersection(pD, pG)
         
     def get_intersection_over_union(pD, pG):
         try:
-            return get_intersection(pD, pG) / get_union(pD, pG);
+            return get_intersection(pD, pG) / get_union(pD, pG)
         except:
             return 0
         
@@ -154,14 +154,14 @@ def evaluate_method(gtFilePath, submFilePath, evaluationParams):
     
     Rectangle = namedtuple('Rectangle', 'xmin ymin xmax ymax')
     
-    gt = rrc_evaluation_funcs.load_zip_file(gtFilePath,evaluationParams['GT_SAMPLE_NAME_2_ID'])
-    subm = rrc_evaluation_funcs.load_zip_file(submFilePath,evaluationParams['DET_SAMPLE_NAME_2_ID'], True)
+    gt = rrc_evaluation_funcs.load_zip_file(gtFilePath, evaluationParams['GT_SAMPLE_NAME_2_ID'])
+    subm = rrc_evaluation_funcs.load_zip_file(submFilePath, evaluationParams['DET_SAMPLE_NAME_2_ID'], True)
    
-    numGlobalCareGt = 0;
-    numGlobalCareDet = 0;
+    numGlobalCareGt = 0
+    numGlobalCareDet = 0
     
-    arrGlobalConfidences = [];
-    arrGlobalMatches = [];
+    arrGlobalConfidences = []
+    arrGlobalMatches = []
 
     for resFile in gt:
         
@@ -215,7 +215,7 @@ def evaluate_method(gtFilePath, submFilePath, evaluationParams):
             
             detFile = rrc_evaluation_funcs.decode_utf8(subm[resFile]) 
             
-            pointsList,confidencesList, _ = rrc_evaluation_funcs.get_tl_line_values_from_file_contents(detFile, evaluationParams['CRLF'], evaluationParams['LTRB'], False, evaluationParams['CONFIDENCES'])
+            pointsList, confidencesList, _ = rrc_evaluation_funcs.get_tl_line_values_from_file_contents(detFile, evaluationParams['CRLF'], evaluationParams['LTRB'], False, evaluationParams['CONFIDENCES'])
             for n in range(len(pointsList)):
                 points = pointsList[n]
                 
@@ -226,13 +226,13 @@ def evaluate_method(gtFilePath, submFilePath, evaluationParams):
                     detPol = polygon_from_points(points)                    
                 detPols.append(detPol)
                 detPolPoints.append(points)
-                if len(gtDontCarePolsNum) > 0 :
+                if len(gtDontCarePolsNum) > 0:
                     for dontCarePol in gtDontCarePolsNum:
                         dontCarePol = gtPols[dontCarePol]
                         intersected_area = get_intersection(dontCarePol, detPol)
                         pdDimensions = detPol.area()
                         precision = 0 if pdDimensions == 0 else intersected_area / pdDimensions
-                        if (precision > evaluationParams['AREA_PRECISION_CONSTRAINT'] ):
+                        if (precision > evaluationParams['AREA_PRECISION_CONSTRAINT']):
                             detDontCarePolsNum.append(len(detPols) - 1)
                             break
                                 
@@ -248,7 +248,7 @@ def evaluate_method(gtFilePath, submFilePath, evaluationParams):
                     for detNum in range(len(detPols)):
                         pG = gtPols[gtNum]
                         pD = detPols[detNum]
-                        iouMat[gtNum,detNum] = get_intersection_over_union(pD, pG)
+                        iouMat[gtNum, detNum] = get_intersection_over_union(pD, pG)
 
                 for gtNum in range(len(gtPols)):
                     for detNum in range(len(detPols)):
@@ -263,7 +263,7 @@ def evaluate_method(gtFilePath, submFilePath, evaluationParams):
 
             if evaluationParams['CONFIDENCES']:
                 for detNum in range(len(detPols)):
-                    if detNum not in detDontCarePolsNum :
+                    if detNum not in detDontCarePolsNum:
                         #we exclude the don't care detections
                         match = detNum in detMatchedNums
 
@@ -283,7 +283,7 @@ def evaluate_method(gtFilePath, submFilePath, evaluationParams):
             recall = float(detMatched) / numGtCare
             precision = 0 if numDetCare == 0 else float(detMatched) / numDetCare
             if evaluationParams['CONFIDENCES'] and evaluationParams['PER_SAMPLE_RESULTS']:
-                sampleAP = compute_ap(arrSampleConfidences, arrSampleMatch, numGtCare )                    
+                sampleAP = compute_ap(arrSampleConfidences, arrSampleMatch, numGtCare)                    
 
         hmean = 0 if (precision + recall) == 0 else 2.0 * precision * recall / (precision + recall)                
 
@@ -314,10 +314,10 @@ def evaluate_method(gtFilePath, submFilePath, evaluationParams):
 
     methodRecall = 0 if numGlobalCareGt == 0 else float(matchedSum) / numGlobalCareGt
     methodPrecision = 0 if numGlobalCareDet == 0 else float(matchedSum) / numGlobalCareDet
-    methodHmean = 0 if methodRecall + methodPrecision==0 else 2 * methodRecall * methodPrecision / (methodRecall + methodPrecision)
+    methodHmean = 0 if methodRecall + methodPrecision == 0 else 2 * methodRecall * methodPrecision / (methodRecall + methodPrecision)
     methodMetrics = {'precision': methodPrecision, 'recall': methodRecall, 'hmean': methodHmean, 'AP': AP}
     resDict = {'calculated': True, 'Message': '', 'method': methodMetrics, 'per_sample': perSampleMetrics}
-    return resDict;
+    return resDict
 
 if __name__ == '__main__':
         
