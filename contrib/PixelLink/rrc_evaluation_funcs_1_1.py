4f33ecb0cacddef8daaefe8753b9750e416decae
王胤哲 <wyz_hbqhd_0916@163.com>
2021-10-28 09:36:44 +0000

    update contrib/PixelLink/rrc_evaluation_funcs_1_1.py.

--- contrib/PixelLink/rrc_evaluation_funcs_1_1.py
+++ contrib/PixelLink/rrc_evaluation_funcs_1_1.py
@@ -97,8 +97,7 @@ def decode_utf8(raw):
     try:
         return raw.decode('utf-8-sig', errors = 'replace')
     except:
-       return None
-   
+        return None
 
 def validate_lines_in_file(fileName, file_contents, CRLF=True, LTRB=True, withTranscription=False, withConfidence=False, imWidth=0, imHeight=0):
     """
