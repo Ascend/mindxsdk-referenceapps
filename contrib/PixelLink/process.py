302c10391519a68457f1a544781178619d969cbe
王胤哲 <wyz_hbqhd_0916@163.com>
2021-11-01 03:08:54 +0000

    update contrib/PixelLink/process.py.

--- contrib/PixelLink/process.py
+++ contrib/PixelLink/process.py
@@ -193,7 +193,7 @@ def rect_to_xys(rect, image_shape):
 
 
 def mask_to_bboxes(mask, image_shape=None, min_area=None,
-                  min_height=None, min_aspect_ratio=None):
+                  min_height=None):
     image_h, image_w = image_shape[0: 2]
     if min_area is None:
         min_area = 300
@@ -222,7 +222,7 @@ def mask_to_bboxes(mask, image_shape=None, min_area=None,
 
 def to_txt(txt_path, image_name,
            image_data, pixel_pos_scores, link_pos_scores):
-    def write_result_as_txt(image_name, bboxes, path):
+    def write_result_as_txt(image_name, bboxes):
         filename = 'res_' + image_name + '.txt'
         lines = []
         for b_idx, bbox in enumerate(bboxes):
@@ -235,7 +235,7 @@ def to_txt(txt_path, image_name,
 
     mask = decode_batch(pixel_pos_scores, link_pos_scores)[0, ...]
     bboxes = mask_to_bboxes(mask, image_data)
-    write_result_as_txt(image_name, bboxes, txt_path)
+    write_result_as_txt(image_name, bboxes)
 
 
 def test(outputFolder, i, image_data_shape, pixel_pos_scores, link_pos_scores):
@@ -247,7 +247,7 @@ def deal(pixel_pos_scores, link_pos_scores):
     # 输出txt目标位置
     outputFolder = './test'
     if not os.path.exists(outputFolder):
-            os.mkdir(outputFolder)
+        os.mkdir(outputFolder)
     image_data_shape = (720, 1280, 3)
 # test only need one process
     for i in range(1, 2):
