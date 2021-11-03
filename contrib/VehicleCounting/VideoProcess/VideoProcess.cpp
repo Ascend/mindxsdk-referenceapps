92e892084df0ca264e630fc0cab7f9c8b3e5c111
吴金泽 <1666371476@qq.com>
2021-11-03 05:09:18 +0000

    update contrib/VehicleCounting/VideoProcess/VideoProcess.cpp.

--- contrib/VehicleCounting/VideoProcess/VideoProcess.cpp
+++ contrib/VehicleCounting/VideoProcess/VideoProcess.cpp
@@ -19,7 +19,6 @@
 #include <sstream>
 #include <queue>
 #include <list>
-#include "MxBase/ErrorCode/ErrorCodes.h"
 #include "MxBase/Log/Log.h"
 #include "opencv2/opencv.hpp"
 #include "VideoProcess.h"
