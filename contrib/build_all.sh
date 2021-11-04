5919e9c5ce35c2b2262e5a40ddb4704141c594c4
墨格杉 <2215704382@qq.com>
2021-11-04 10:59:25 +0000

    update contrib/build_all.sh.

--- contrib/build_all.sh
+++ contrib/build_all.sh
@@ -19,6 +19,18 @@ current_folder="$( cd "$(dirname "$0")" ;pwd -P )"
 
 SAMPLE_FOLDER=(
     FaceEmotionRecognition/
+    ActionRecognition/
+	CrowdCounting/
+    mxBase_wheatDetection/
+	EdgeDetectionPicture/
+    HelmetIdentification/
+    Individual/
+    human_segmentation/
+	OpenposeKeypointDetection/
+	PersonCount/
+	FatigueDrivingRecognition/
+	CartoonGANPicture/
+	HeadPoseEstimation/
 )
 
 
