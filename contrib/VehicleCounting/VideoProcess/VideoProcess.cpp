0da22fc107885442e21d12bfa32b103e71b7e546
吴金泽 <1666371476@qq.com>
2021-11-04 15:23:23 +0000

    update contrib/VehicleCounting/VideoProcess/VideoProcess.cpp.

--- contrib/VehicleCounting/VideoProcess/VideoProcess.cpp
+++ contrib/VehicleCounting/VideoProcess/VideoProcess.cpp
@@ -23,12 +23,11 @@
 #include "MxBase/Log/Log.h"
 #include "opencv2/opencv.hpp"
 #include "VideoProcess.h"
-extern "C" {
-    #include "libavformat/avformat.h"
-    #include "libavcodec/avcodec.h"
-    #include "libavutil/avutil.h"
-    #include "libswscale/swscale.h"
-}
+
+#include "libavformat/avformat.h"
+#include "libavcodec/avcodec.h"
+#include "libavutil/avutil.h"
+#include "libswscale/swscale.h"
 
 namespace {
     static std::vector<std::queue<center>> pts(10000); // 保存每个车辆轨迹的最新的20个bbox的中心点
