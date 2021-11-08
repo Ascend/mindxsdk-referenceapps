a92862966c082badae103f1b3e8cac4befb27f70
张玉炎 <1697204239@qq.com>
2021-11-08 02:11:23 +0000

    第一次上传

---
+++ contrib/FairMOT/FairmotPostProcess/FairmotPostProcess.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright(C) 2021. Huawei Technologies Co.,Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#ifndef YOLOV3_POST_PROCESS_H
+#define YOLOV3_POST_PROCESS_H
+#include "MxBase/PostProcessBases/ObjectPostProcessBase.h"
+#include "opencv2/opencv.hpp"
+
+namespace MxBase {
+    class FairmotPostProcess: public ObjectPostProcessBase
+    {
+    public:
+        FairmotPostProcess() = default;
+
+        ~FairmotPostProcess() = default;
+
+        FairmotPostProcess(const FairmotPostProcess &other) = default;
+
+        APP_ERROR Init(const std::map <std::string, std::shared_ptr<void>> &postConfig) override;
+
+        APP_ERROR DeInit() override;
+
+        APP_ERROR Process(const std::vector <MxBase::TensorBase> &tensors,
+                          std::vector <std::vector<MxBase::ObjectInfo>> &objectInfos,
+                          const std::vector <MxBase::ResizedImageInfo> &resizedImageInfos = {},
+                          const std::map <std::string, std::shared_ptr<void>> &paramMap = {}) override;
+
+    protected:
+        
+        bool IsValidTensors(const std::vector <MxBase::TensorBase> &tensors) const override;
+
+        void ObjectDetectionOutput(const std::vector <MxBase::TensorBase> &tensors,
+                                   std::vector <std::vector<MxBase::ObjectInfo>> &objectInfos,
+                                   const std::vector <MxBase::ResizedImageInfo> &resizedImageInfos = {});
+
+    };
+    extern "C" {
+    std::shared_ptr<MxBase::FairmotPostProcess> GetObjectInstance();
+    }
+}
+#endif
