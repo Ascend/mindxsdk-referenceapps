1938d6322ef33c027dd06384e815244f414dbfb2
张玉炎 <1697204239@qq.com>
2021-11-09 06:24:15 +0000

    update contrib/build_all.sh.

--- contrib/build_all.sh
+++ contrib/build_all.sh
@@ -18,6 +18,19 @@ current_folder="$( cd "$(dirname "$0")" ;pwd -P )"
 
 
 SAMPLE_FOLDER=(
+    ActionRecognition/
+	CrowdCounting/
+    mxBase_wheatDetection/
+	EdgeDetectionPicture/
+    HelmetIdentification/
+    Individual/
+    human_segmentation/
+	OpenposeKeypointDetection/
+	PersonCount/
+	FatigueDrivingRecognition/
+	CartoonGANPicture/
+	HeadPoseEstimation/
+	BertTextClassification/
     FairMOT/
 )
 
