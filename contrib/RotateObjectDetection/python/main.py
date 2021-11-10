6364084950b4cc35985f4578c94b31e1e5ca4906
崔翔 <2576313217@qq.com>
2021-11-10 07:51:31 +0000

    update contrib/RotateObjectDetection/python/main.py.

--- contrib/RotateObjectDetection/python/main.py
+++ contrib/RotateObjectDetection/python/main.py
@@ -14,27 +14,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import os
 import cv2
 import numpy as np
 import sys
-sys.path.append("../proto")
-import mxpiRotateobjProto_pb2 as mxpiRotateObjProto
-import MxpiDataType_pb2 as MxpiDataType
 import argparse
+import MxpiDataType_pb2 as MxpiDataType
 
 from StreamManagerApi import StreamManagerApi, MxDataInput, StringVector
+sys.path.append("../proto")
+import mxpiRotateobjProto_pb2 as mxpiRotateObjProto
+
+
 
 if __name__ == '__main__':
     streamManagerApi = StreamManagerApi()
-    # 新建一个流管理StreamManager对象并初始化
+    # Create a new StreamManager object and initialize it.
     ret = streamManagerApi.InitManager()
     if ret != 0:
         print("Failed to init Stream manager, ret=%s" % str(ret))
         exit()
 
-    # 构建pipeline
+    # Create a pipeline.
     with open("./pipeline/RotateObjectDetection.pipeline", "rb") as f:
         pipeline_str = f.read()
     ret = streamManagerApi.CreateMultipleStreams(pipeline_str)
@@ -50,38 +51,40 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     print(opt)
 
+    # The images to be tested are stored in this folder.
     input_path = opt.input_path
+    # Test results are placed in this folder.
     out_path = opt.output_path
-    labels = opt.labels
-    cont = 0
+    # Whether to print labels for image.
+    labels = opt.labels 
+    # Record the current image detection order.
+    cont = 0 
 
+    # Check whether the input path is empty.
     if len(os.listdir(input_path)) == 0:
         print("The image folder is empty.") 
 
+    # Walk through each image in the image folder
     for item in os.listdir(input_path):
         img_path = os.path.join(input_path, item)
         print("Detect the order of the image: {}".format(cont))
         print("detect_file_path:", img_path)
     
-        # 构建流的输入对象--检测目标
+        # Build the input object for the stream.
         dataInput = MxDataInput()
-        
-        if os.path.exists(input_path) != 1:
-            print("The test image does not exist.")
 
         with open(img_path, 'rb') as f:
             dataInput.data = f.read()
-
-        
+       
         streamName = b'detection'
         inPluginId = 0
         
-        # 根据流名将检测目标传入流中
+        # Pass the detection image into the stream.
         uniqueId = streamManagerApi.SendData(streamName, inPluginId, dataInput)
         if uniqueId < 0:
             print("Failed to send data to stream.")
             exit()
-
+ 
         keyVec = StringVector()
         keys = (b"mxpi_imagedecoder0", b"mxpi_rotateobjproto")
         for key in keys:
@@ -98,7 +101,7 @@ if __name__ == '__main__':
                 infer_result[0].errorCode))
             exit()
 
-        # mxpi_imagedecoder0 图像解码插件输出信息
+        # output information of imagedecoder plugin output information
         YUV_BYTES_NU = 3
         YUV_BYTES_DE = 2
         visionList = MxpiDataType.MxpiVisionList()
@@ -106,12 +109,12 @@ if __name__ == '__main__':
         vision_data = visionList.visionVec[0].visionData.dataStr
         visionInfo = visionList.visionVec[0].visionInfo
 
-        # 用输出原件信息初始化OpenCV图像信息矩阵
         img_yuv = np.frombuffer(vision_data, np.uint8)
         img_bgr = img_yuv.reshape(visionInfo.heightAligned * YUV_BYTES_NU // YUV_BYTES_DE, 
                                     visionInfo.widthAligned)
         img = cv2.cvtColor(img_bgr, getattr(cv2, "COLOR_YUV2BGR_NV12"))
         
+        # output information of postprocess plugin
         result_protolist = mxpiRotateObjProto.MxpiRotateobjProtoList()
         result_protolist.ParseFromString(infer_result[1].messageBuf)
 
@@ -125,30 +128,33 @@ if __name__ == '__main__':
                         'small-vehicle', 'large-vehicle', 'ship', 'tennis-court',
                         'basketball-court', 'storage-tank', 'soccer-ball-field', 'roundabout', 
                         'harbor', 'swimming-pool', 'helicopter', 'container-crane']
-        s = ' '
+        s = ''
         tl = 1
         tf = tl - 1 if (tl - 1 > 1) else 1
         
-        for i in range(len(result_protovec)):
+        for vec in result_protovec:
             rbox = dict(zip(keys, values))
-            rbox["x_c"] = result_protovec[i].x_c
-            rbox["y_c"] = result_protovec[i].y_c
-            rbox["width"] = result_protovec[i].width
-            rbox["height"] = result_protovec[i].height
-            rbox["angle"] = result_protovec[i].angle
-            rbox["confidence"] = result_protovec[i].confidence
-            rbox["classID"] = result_protovec[i].classID
-            rbox["className"] = result_protovec[i].className
-            # print(rbox)
+            rbox["x_c"] = vec.x_c
+            rbox["y_c"] = vec.y_c
+            rbox["width"] = vec.width
+            rbox["height"] = vec.height
+            rbox["angle"] = vec.angle
+            rbox["confidence"] = vec.confidence
+            rbox["classID"] = vec.classID
+            rbox["className"] = vec.className
+            # Add postprocessing information to list rboxes.
             rboxes.append(rbox)
-            classID_list.append(result_protovec[i].classID)
+            # Add classID of each object to classID_list.
+            classID_list.append(vec.classID)
         
+        # Count the number of detected objects.
         for c in np.unique(np.array(classID_list)):
             n = (np.array(classID_list) == c).sum()
             s += '%d %s, ' % (n, classNames[int(c)])
         s = s[:-2] if s else "No object is detectde"
         print("detect result: ", s)
         
+        # Output the information about the bbox to .txt files.
         text_out_path = out_path + '/' + 'result_txt/result_before_merge/'
         oriname = item.split('__')[0]
         for rbox in rboxes:
@@ -162,25 +168,27 @@ if __name__ == '__main__':
             with open(str(text_out_path + oriname) + '.txt', 'a') as f:
                 f.writelines(lines + '\n')
 
-
-        np.random.seed(666)
+        # Set the random seed to 666 and randomly generate 16 colors.
+        RANDOM_SEED = 666
+        np.random.seed(RANDOM_SEED)
         colors = [[np.random.randint(0, 255) for _ in range(3)] for _ in range(classNum)]
 
-        for i in range(len(rboxes)):
-            rect = [(rboxes[i]["x_c"], rboxes[i]["y_c"]), (rboxes[i]["width"], 
-                        rboxes[i]["height"]), rboxes[i]["angle"]]
+        # Visual detection information.
+        for rbox in rboxes:
+            rect = [(rbox["x_c"], rbox["y_c"]), (rbox["width"], 
+                        rbox["height"]), rbox["angle"]]
             poly = cv2.boxPoints(rect)
             poly = np.int0(poly)
             cv2.drawContours(image=img, contours=[poly], contourIdx=-1, 
-                            color=colors[int(rboxes[i]["classID"])], thickness=2 * tl)
+                            color=colors[int(rbox["classID"])], thickness=2 * tl)
             if labels:
-                label = '%s %.2f' % (rboxes[i]["className"], rboxes[i]["confidence"])
+                label = '%s %.2f' % (rbox["className"], rbox["confidence"])
             else:
-                label = '%s' % rboxes[i]["classID"]
-            c1 = (int(rboxes[i]["x_c"]), int(rboxes[i]["y_c"]))
+                label = '%s' % rbox["classID"]
+            c1 = (int(rbox["x_c"]), int(rbox["y_c"]))
             t_size = cv2.getTextSize(label, 0, fontScale=tl / 4, thickness=tf)[0]
             c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-            cv2.rectangle(img, c1, c2, colors[int(rboxes[i]["classID"])], -1, cv2.LINE_AA)  
+            cv2.rectangle(img, c1, c2, colors[int(rbox["classID"])], -1, cv2.LINE_AA)  
             cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 4, [225, 255, 255], 
                         thickness=tf, lineType=cv2.LINE_AA)
             
