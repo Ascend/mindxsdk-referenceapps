ad49ce905753b204f01450fbac70e5ca146f4c27
张玉炎 <1697204239@qq.com>
2021-11-10 07:27:34 +0000

    update contrib/FairMOT/plugins/MxpiTrackIdReplaceClassName/MxpiTrackIdReplaceClassName.h.

--- contrib/FairMOT/plugins/MxpiTrackIdReplaceClassName/MxpiTrackIdReplaceClassName.h
+++ contrib/FairMOT/plugins/MxpiTrackIdReplaceClassName/MxpiTrackIdReplaceClassName.h
@@ -1,18 +1,18 @@
 /*
-* Copyright(C) 2020. Huawei Technologies Co.,Ltd. All rights reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright(C) 2021. Huawei Technologies Co.,Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef SDKMEMORY_MXPITRACKIDREPLACECLASSNAME_H
 #define SDKMEMORY_MXPITRACKIDREPLACECLASSNAME_H
@@ -57,7 +57,7 @@ public:
     static std::vector<std::shared_ptr<void>> DefineProperties();
     /**
      * @api
-     * @brief Get the number of class id and confidence from model inference.
+     * @brief Replace className from mxpi_objectpostprocessor with trackId from mxpi_motsimplesortV2
      * @param key
      * @param buffer
      * @return APP_ERROR
