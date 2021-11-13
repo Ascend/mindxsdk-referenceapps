ce2f213c715da50ae99962ad9e15050de1dff4d1
崔翔 <2576313217@qq.com>
2021-11-13 01:31:49 +0000

    update contrib/RotateObjectDetection/DOTA_devkit/ResultMergeDraw.py.

--- contrib/RotateObjectDetection/DOTA_devkit/ResultMergeDraw.py
+++ contrib/RotateObjectDetection/DOTA_devkit/ResultMergeDraw.py
@@ -23,114 +23,13 @@
 import os
 import numpy as np
 import re
-from utils import polyiou
+from utils import general_utils
 import copy
 import cv2
+import argparse
 
 ## the IoU thresh for nms when merge image
 nms_thresh = 0.3
-
-def py_cpu_nms_poly(dets, thresh):
-    """
-    任意四点poly nms.取出nms后的边框的索引
-    @param dets: shape(detection_num, [poly, confidence1]) 原始图像中的检测出的目标数量
-    @param thresh:
-    @return:
-            keep: 经nms后的目标边框的索引
-    """  
-    scores = dets[:, 8]
-    polys = []
-    # for i in range(len(dets)):
-    #     tm_polygon = [dets[i][0], dets[i][1],
-    #                   dets[i][2], dets[i][3],
-    #                   dets[i][4], dets[i][5],
-    #                   dets[i][6], dets[i][7]]
-    #     polys.append(tm_polygon)
-
-    for det in dets:
-        tm_polygon = [det[0], det[1],
-                      det[2], det[3],
-                      det[4], det[5],
-                      det[6], dets[7]]
-        polys.append(tm_polygon)
-
-    # argsort将元素小到大排列 返回索引值 [::-1]即从后向前取元素
-    order = scores.argsort()[::-1]  # 取出元素的索引值 顺序为从大到小
-    keep = []
-    while order.size > 0:
-        ovr = []
-        i = order[0]  # 取出当前剩余置信度最大的目标边框的索引
-        keep.append(i)
-        for j in range(order.size - 1):  # 求出置信度最大poly与其他所有poly的IoU
-            iou = polyiou.iou_poly(polys[i], polys[order[j + 1]])
-            ovr.append(iou)
-        ovr = np.array(ovr)
-        inds = np.where(ovr <= thresh)[0]  # 找出iou小于阈值的索引
-        order = order[inds + 1]
-    return keep
-
-def nmsbynamedict(nameboxdict, nameboxdict_classname, nms, thresh):
-    """
-    对namedict中的目标信息进行nms.不改变输入的数据形式
-    @param nameboxdict: eg:{
-                           'P706':[[poly1, confidence1], ..., 
-                                   [poly9, confidence9]],
-                           ...
-                           'P700':[[poly1, confidence1], ..., 
-                                   [poly9, confidence9]]
-                            }
-    @param nameboxdict_classname: eg:{
-                           'P706':[[poly1, confidence1,'classname'], ..., 
-                                   [poly9, confidence9, 'classname']],
-                           ...
-                           'P700':[[poly1, confidence1, 'classname'], ..., 
-                                   [poly9, confidence9, 'classname']]
-                            }
-    @param nms:
-    @param thresh: nms阈值, IoU阈值
-    @return:
-            nameboxnmsdict: eg:{
-                                'P706':[[poly1, confidence1, 'classname'], ..., 
-                                        [poly_nms, confidence9, 'classname']],
-                                 ...
-                                'P700':[[poly1, confidence1, 'classname'], ..., 
-                                        [poly_nms, confidence9, 'classname']]
-                               }
-    """
-    # 初始化字典
-    nameboxnmsdict = {x: [] for x in nameboxdict}  # eg: nameboxnmsdict={'P0770': [], 'P1888': []}
-    for imgname in nameboxdict:  # 提取nameboxdict中的key eg:P0770   P1888
-        keep = nms(np.array(nameboxdict[imgname]), thresh)  # rotated_nms索引值列表
-        outdets = []
-        for index in keep:
-            outdets.append(nameboxdict_classname[imgname][index])
-        nameboxnmsdict[imgname] = outdets
-    return nameboxnmsdict
-
-def poly2origpoly(poly, x, y, rate):
-    origpoly = []
-    for i in range(int(len(poly)/2)):
-        tmp_x = float(poly[i * 2] + x) / float(rate)
-        tmp_y = float(poly[i * 2 + 1] + y) / float(rate)
-        origpoly.append(tmp_x)
-        origpoly.append(tmp_y)
-    return origpoly
-
-def getfilefromthisrootdir(dir, ext=None):
-    allfiles = []
-    needExtFilter = (ext is not None)
-    for root, dirs, files in os.walk(dir):
-        for filespath in files:
-            filepath = os.path.join(root, filespath)
-            extension = os.path.splitext(filepath)[1][1:]
-            if needExtFilter and extension in ext:
-                allfiles.append(filepath)
-            elif not needExtFilter:
-                allfiles.append(filepath)
-    return allfiles     
-
-def custombasename(fullname):
-    return os.path.basename(os.path.splitext(fullname)[0])
 
 def mergebase(srcpath, dstpath, nms):
     """
@@ -140,9 +39,9 @@ def mergebase(srcpath, dstpath, nms):
     @param nms: NMS函数
     """
     # srcpath文件夹下的所有文件相对路径 eg:['example_split/../P0001.txt', ..., '?.txt']
-    filelist = getfilefromthisrootdir(srcpath)  
+    filelist = general_utils.getfilefromthisrootdir(srcpath)  
     for fullname in filelist:  # 'example_split/../P0001.txt'
-        name = custombasename(fullname)  # 只留下文件名 eg:P0001
+        name = general_utils.custombasename(fullname)  # 只留下文件名 eg:P0001
         dstname = os.path.join(dstpath, name + '.txt')  # eg: example_merge/..P0001.txt
         if not os.path.exists(dstpath):
             os.makedirs(dstpath)
@@ -171,7 +70,7 @@ def mergebase(srcpath, dstpath, nms):
                 confidence = splitline[1]
                 classname = splitline[-1]
                 poly = list(map(float, splitline[2:10]))  # 每个元素映射为浮点数 再放入列表中
-                origpoly = poly2origpoly(poly, x, y, rate)  # 将目标位置信息resize 恢复成原图的poly坐标
+                origpoly = general_utils.poly2origpoly(poly, x, y, rate)  # 将目标位置信息resize 恢复成原图的poly坐标
                 det = origpoly  # shape(8)
                 det.append(confidence)  # [poly, confidence]
                 det = list(map(float, det))
@@ -185,7 +84,7 @@ def mergebase(srcpath, dstpath, nms):
                 nameboxdict[oriname].append(det)
                 nameboxdict_classname[oriname].append(det_classname)
             # 对nameboxdict元组进行nms
-            nameboxnmsdict = nmsbynamedict(nameboxdict, nameboxdict_classname, nms, nms_thresh)  
+            nameboxnmsdict = general_utils.nmsbynamedict(nameboxdict, nameboxdict_classname, nms, nms_thresh)  
             with open(dstname, 'w') as f_out:
                 for imgname in nameboxnmsdict:
                     for det in nameboxnmsdict[imgname]:  # 取出对应图片的nms后的目标信息
@@ -193,7 +92,7 @@ def mergebase(srcpath, dstpath, nms):
                         bbox = det[0:-2]
                         outline = imgname + ' ' + str(confidence) + ' ' + ' '.join(map(str, bbox)) + ' ' + det[-1]
                         f_out.write(outline + '\n')
-            print(fullname + " merge down!")
+            print(name + " merge down!")
 
 def mergebypoly(srcpath, dstpath):
     """
@@ -202,9 +101,9 @@ def mergebypoly(srcpath, dstpath):
     """
     mergebase(srcpath,
               dstpath,
-              py_cpu_nms_poly)
+              general_utils.py_cpu_nms_poly)
 
-def draw_dota_image(imgsrcpath, imglabelspath, dstpath, extractclassname, thickness):
+def draw_dota_image(imgsrcpath, imglabelspath, dstpath, extractclassname, thickness, labels):
     """
     绘制工具merge后的目标/DOTA GT图像
         @param imgsrcpath: merged后的图像路径(原始图像路径)
@@ -215,10 +114,9 @@ def draw_dota_image(imgsrcpath, imglabelspath, dstpath, extractclassname, thickn
     if not os.path.exists(dstpath):
         os.makedirs(dstpath)
     # 设置画框的颜色    
-    # colors = [[178, 63, 143], [25, 184, 176], [238, 152, 129],....,[235, 137, 120]]随机设置RGB颜色
     np.random.seed(666)
     colors = [[np.random.randint(0, 255) for _ in range(3)] for _ in range(len(extractclassname))]
-    filelist = getfilefromthisrootdir(imglabelspath)  # fileist=['/.../P0005.txt', ..., /.../P000?.txt]
+    filelist = general_utils.getfilefromthisrootdir(imglabelspath)  # fileist=['/.../P0005.txt', ..., /.../P000?.txt]
     for fullname in filelist:  # fullname='/.../P000?.txt'
         objects = []
         with open(fullname, 'r') as f_in:  # 打开merge后/原始的DOTA图像的gt.txt
@@ -229,12 +127,12 @@ def draw_dota_image(imgsrcpath, imglabelspath, dstpath, extractclassname, thickn
                 del splitlines[0]
                 del splitlines[0]   # 删除前两个无用信息
                 objects = [x[0:-2] for x in splitlines]
-                classnames = [x[-2] for x in splitlines]
+                class_names = [x[-2] for x in splitlines]
             else:
                 # P0003 0.911 660.0 309.0 639.0 209.0 661.0 204.0 682.0 304.0 large-vehicle
                 confidences = [float(x[1]) for x in splitlines]
                 objects = [x[2:-1] for x in splitlines]
-                classnames = [x[-1] for x in splitlines]
+                class_names = [x[-1] for x in splitlines]
 
         '''
         objects[i] = str[poly, classname]
@@ -248,7 +146,7 @@ def draw_dota_image(imgsrcpath, imglabelspath, dstpath, extractclassname, thickn
         tf = tl - 1 if (tl - 1 > 1) else 1
 
         for i, obj in enumerate(objects):
-            classname = classnames[i]
+            classname = class_names[i]
             poly = np.array(list(map(float, obj)))
             poly = poly.reshape(4, 2)  # 返回rect对应的四个点的值 normalized
             poly = np.int0(poly)
@@ -259,8 +157,10 @@ def draw_dota_image(imgsrcpath, imglabelspath, dstpath, extractclassname, thickn
                              contourIdx=-1,
                              color=colors[int(extractclassname.index(classname))],
                              thickness=thickness)
-
-            label = '%s %.2f' % (classnames[i], confidences[i])
+            if labels:
+                label = '%s %.2f' % (classnames[i], confidences[i])
+            else:
+                label = '%s' % int(extractclassname.index(classname))
             rec = cv2.minAreaRect(poly)
             c1 = (int(rec[0][0]), int(rec[0][1]))
             t_size = cv2.getTextSize(label, 0, fontScale=tl / 4, thickness=tf)[0]
@@ -271,17 +171,24 @@ def draw_dota_image(imgsrcpath, imglabelspath, dstpath, extractclassname, thickn
         cv2.imwrite(img_savename, img)
         
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--labels', action='store_true', default=False, help='whether to print labels')    
+    opt = parser.parse_args()
+    labels = opt.labels
+
     classnames = ['plane', 'baseball-diamond', 'bridge', 'ground-track-field', 
                   'small-vehicle', 'large-vehicle', 'ship', 'tennis-court',
                   'basketball-court', 'storage-tank', 'soccer-ball-field', 'roundabout', 
                   'harbor', 'swimming-pool', 'helicopter', 'container-crane']
 
-    mergebypoly(r'/home/zhongzhi8/RotatedObjectDetection/testdetection/result_txt/result_before_merge', 
-                r'/home/zhongzhi8/RotatedObjectDetection/testdetection/result_txt/result_merged')
+    mergebypoly(r'../detection/result_txt/result_before_merge', 
+                r'../detection/result_txt/result_merged')
 
-    draw_dota_image(imgsrcpath=r'/home/zhongzhi8/RotatedObjectDetection/testImage',
-                    imglabelspath=r'/home/zhongzhi8/RotatedObjectDetection/testdetection/result_txt/result_merged',
-                    dstpath=r'/home/zhongzhi8/RotatedObjectDetection/testdetection/merged_drawed',
+    draw_dota_image(imgsrcpath=r'/home/zhongzhi8/RotatedObjectDetection/dataSet/images',
+                    imglabelspath=r'../detection/result_txt/result_merged',
+                    dstpath=r'../detection/merged_drawed',
                     extractclassname=classnames,
-                    thickness=2
+                    thickness=2,
+                    labels=labels
                     )
