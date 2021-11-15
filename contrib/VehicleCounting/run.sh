5edfadbb21abec735c1fd93820ccd0e9380107e9
吴金泽 <1666371476@qq.com>
2021-11-15 11:25:45 +0000

    update contrib/VehicleCounting/run.sh.

--- contrib/VehicleCounting/run.sh
+++ contrib/VehicleCounting/run.sh
@@ -12,10 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-path_cur="/VehicleCounting"
-CUR_PATH=$(cd "$path_cur" || exit ; pwd)
-
-echo "$CUR_PATH"
 export MX_SDK_HOME={SDK实际安装路径}
 export FFMPEG_PATH={ffmpeg实际安装路径}
 export LD_LIBRARY_PATH=${MX_SDK_HOME}/lib:${MX_SDK_HOME}/opensource/lib:${MX_SDK_HOME}/opensource/lib64:/usr/local/Ascend/ascend-toolkit/latest/acllib/lib64:/usr/local/Ascend/driver/lib64/:/usr/local/python3.7.5/lib:${FFMPEG_PATH}/lib:${LD_LIBRARY_PATH}
