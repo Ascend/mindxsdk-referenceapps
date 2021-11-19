583f82a10c05d1069f901304ffbbe995de91a0b2
sha-xiaobao <1402995230@qq.com>
2021-11-19 16:15:04 +0800

    [电子科技大学][MMNET人像分割参考设计]

--- contrib/MMNET/main.py
+++ contrib/MMNET/main.py
@@ -19,6 +19,8 @@ from StreamManagerApi import StreamManagerApi, MxDataInput, StringVector
 import MxpiDataType_pb2 as MxpiDataType
 import numpy as np
 import cv2
+import os
+from PIL import Image
 
 if __name__ == '__main__':
     # init stream manager
@@ -29,19 +31,43 @@ if __name__ == '__main__':
         exit()
 
     # create streams by pipeline config file
-    with open("../pipeline/MMNET.pipeline", 'rb') as f:
+    with open("./pipeline/MMNET.pipeline", 'rb') as f:
         pipelineStr = f.read()
     ret = streamManagerApi.CreateMultipleStreams(pipelineStr)
     if ret != 0:
         print("Failed to create Stream, ret=%s" % str(ret))
         exit()
-
-    # Construct the input of the stream
+        
+    # Construct the input of the stream & check the input image
+    min_image_size = 32
+    max_image_size = 8192
     dataInput = MxDataInput()
     filepath = "test.jpg"
-    with open(filepath, 'rb') as f:
-        dataInput.data = f.read()
-
+    if os.path.exists(filepath) != 1:
+        print("Failed to get the input picture. Please check it!")
+        exit()
+    else:
+        try:
+            image = Image.open(filepath)
+            if image.format != 'JPEG':
+                print('input image only support jpg, curr format is {}.'.format(image.format))
+                exit()
+            elif image.width < min_image_size or image.width > max_image_size:
+                print('input image width must in range [{}, {}], curr width is {}.'.format(
+                    min_image_size, max_image_size, image.width))
+                exit()
+            elif image.height < min_image_size or image.height > max_image_size:
+                print('input image height must in range [{}, {}], curr height is {}.'.format(
+                    min_image_size, max_image_size, image.height))
+                exit()
+            else:
+                input_valid = True
+                # read input image
+                with open(filepath, 'rb') as f:
+                    dataInput.data = f.read()
+        except IOError:
+            print('an IOError occurred while opening {}, maybe your input is not a picture.'.format(filepath))
+            exit()
 
     # Inputs data to a specified stream based on streamName.
     streamName = b'mmnet'
@@ -69,9 +95,14 @@ if __name__ == '__main__':
     prediction = np.frombuffer(tensorList.tensorPackageVec[0].tensorVec[0].dataStr, dtype = np.float32)
     prediction_shape = tensorList.tensorPackageVec[0].tensorVec[0].tensorShape
     prediction = np.reshape(prediction, prediction_shape)
-    
+
     out_1 = np.reshape(prediction[:, :, :, 1], (256, 256))
-    out_1 = (np.max(out_1) - np.min(out_1)) * 255 * out_1
+    out_1 = 255 * out_1
+
+    # Resize the model output image (256, 256) to the original size
+    img = cv2.imread(filepath)
+    size = img.shape
+    out_1 = cv2.resize(out_1, (size[1], size[0]))
     
     filepath_out = "test-out.jpg"
     cv2.imwrite(filepath_out, out_1)
