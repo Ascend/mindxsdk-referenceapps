1fbdb689d783a08b90581718885786344d351446
吴金泽 <1666371476@qq.com>
2021-11-25 07:28:22 +0000

    update contrib/VehicleCounting/ReadConfig/GetConfig.cpp.

--- contrib/VehicleCounting/ReadConfig/GetConfig.cpp
+++ contrib/VehicleCounting/ReadConfig/GetConfig.cpp
@@ -8,6 +8,7 @@ using namespace std;
 vector<string> v={"video_width","video_height","frame_rate","is_singlelane","lane_num","line_s_x",
                   "line_s_y","line_e_x","line_e_y","is_vertical","point_x","point_y","point1_x",
                   "point1_y","point2_x","point2_y","det_threshold","nms_iouthreshold"};
+vector<string> v1={"is_singlelane","lane_num","is_vertical"};
 bool isnum(string s)
 {
     stringstream sin(s);
@@ -105,10 +106,12 @@ bool ReadConfig(const string & filename, map<string, string> & m)
     string line, key, value;
     while (getline(infile, line)) {
         if (AnalyseLine(line, key, value)) {
+            // 判断参数是否不为空
             if(value.empty()){
                 cout << "parameter "<<key<<" is empty!" << endl;
                 return false;
             }
+            // 判断value是否为合法数字
             if(!isnum(value)){
                 cout << "parameter "<<key<<" is illegal!" << endl;
                 return false;
@@ -116,9 +119,17 @@ bool ReadConfig(const string & filename, map<string, string> & m)
             m[key] = value;
         }
     }
+    // 判断is_singlelane,lane_num,is_vertical取值的合法性
+    for(uint32_t i=0;i<v1.size();i++){
+        if(stoi(m[v1[i]])!=0&&stoi(m[v1[i]])!=1){
+            cout << "parameter "<<v1[i]<<" value is illegal!" << endl;
+            return false;
+        }
+    }
+    // 判断是否读取到每个参数
     for(uint32_t i=0;i<v.size();i++){
         if(m.find(v[i])==m.end()){
-            cout << "parameter "<<key<<" do not exist!" << endl;
+            cout << "parameter "<<v[i]<<" do not exist!" << endl;
             return false;
         }
     }
