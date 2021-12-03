f2d86a5e78c949100c6eda07b877507a8b32749e
王满满 <1910839243@qq.com>
2021-12-03 11:25:56 +0000

    update contrib/UltraFastLaneDetection/main_text.py.

--- contrib/UltraFastLaneDetection/main_text.py
+++ contrib/UltraFastLaneDetection/main_text.py
@@ -15,79 +15,26 @@ if __name__ == '__main__':
     if ret != 0:
         print("Failed to init Stream manager, ret=%s" % str(ret))
         exit()
-    img = '../test6.jpg'  # 修改图片地址
+    img = '../img/7'   # 修改图片地址
+    img_format = '.jpg'
     # 构建pipeline
-    pipeline = {
-        "detection": {
-            "stream_config": {
-                "deviceId": "0"
-            },
-            "appsrc0": {
-                "props": {
-                    "blocksize": "409600"
-                },
-                "factory": "appsrc",
-                "next": "mxpi_imagedecoder0"
-            },
-            "mxpi_imagedecoder0": {
-                "props": {
-                    "deviceId": "0"
-                },
-                "factory": "mxpi_imagedecoder",
-                "next": "mxpi_imageresize0"
-            },
-            "mxpi_imageresize0": {
-                "props": {
-                    "dataSource": "mxpi_imagedecoder0",
-                    "resizeType": "Resizer_KeepAspectRatio_Fit",
-                    "resizeHeight": "288",
-                    "resizeWidth": "800"
-                },
-                "factory": "mxpi_imageresize",
-                "next": "mxpi_tensorinfer0"
-            },
-            "mxpi_tensorinfer0": {
-                "props": {
-                    "dataSource": "mxpi_imageresize0",
-                    "modelPath": "./model/culane_18_2.om"
-                },
-                "factory": "mxpi_tensorinfer",
-                "next": "mxpi_objectpostprocessor0"
-            },
-            "mxpi_objectpostprocessor0": {
-                "props": {
-                    "dataSource": "mxpi_tensorinfer0",
-                    "postProcessConfigPath": "./model/yolov3_tf_bs1_fp16.cfg",
-                    "labelPath": "./model/coco.names",
-                    "postProcessLibPath": "./libLanepostprocess.so"
-                },
-                "factory": "mxpi_objectpostprocessor",
-                "next": "appsink0"
-            },
-            "appsink0": {
-                "props": {
-                    "blocksize": "4096000"
-                },
-                "factory": "appsink"
-            }
-        }
-    }
-
-    pipelineStr = json.dumps(pipeline).encode()
+    with open("../Lane.pipeline", 'rb') as f:
+    pipelineStr = f.read()
     ret = streamManagerApi.CreateMultipleStreams(pipelineStr)
     if ret != 0:
         print("Failed to create Stream, ret=%s" % str(ret))
         exit()
 
     # 构建流的输入对象--检测目标
+    img_path = img + img_format
     dataInput = MxDataInput()
-    if os.path.exists(img) != 1:
+    if os.path.exists(img_path) != 1:
         print("The test image does not exist.")
     try :
-        with open(img, 'rb') as f:
+        with open(img_path, 'rb') as f:
         dataInput.data = f.read()
     except FileNotFoundError:
-        print(img, "doesn't exist. Exit.")
+        print(img_path, "doesn't exist. Exit.")
         exit()
     streamName = b'detection'
     inPluginId = 0
@@ -116,17 +63,21 @@ if __name__ == '__main__':
     out_j = np.array(out).reshape(18,4)
 
     # #后处理___________________________________________________________________________________________________________________
+    vis = cv2.imread(img_path)
+    img_size = vis.shape
     img_w, img_h = 1640, 590
+    vis_resize = cv2.resize(vis, (img_w,img_h), interpolation=cv2.INTER_CUBIC)
     cls_num_per_lane = 18
     row_anchor = [121, 131, 141, 150, 160, 170, 180, 189, 199, 209, 219, 228, 238, 248, 258, 267, 277, 287]
     col_sample = np.linspace(0, 800 - 1, 200)
     col_sample_w = col_sample[1] - col_sample[0]
-
-    vis = cv2.imread(img)
+    res_path = img + '_r' + img_format
     for i in range(out_j.shape[1]):
         if np.sum(out_j[:, i] != 0) > 2:
             for k in range(out_j.shape[0]):
                 if out_j[k, i] > 0:
                     ppp = (int(out_j[k, i] * col_sample_w * img_w / 800) - 1,int(img_h * (row_anchor[cls_num_per_lane - 1 - k] / 288)) - 1)
                     cv2.circle(vis, ppp, 5, (0, 255, 0), -1)
-                    cv2.imwrite(img, vis)
+
+    vis_res = cv2.resize(vis_resize, (img_size[1], img_size[0]), interpolation=cv2.INTER_CUBIC)       
+    cv2.imwrite(res_path, vis_res)
