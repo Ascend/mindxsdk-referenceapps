00c2e1a2ff47e2592968db113ce18bf4d5bf1015
沈航宇 <shenhangyu@tom.com>
2021-12-17 16:20:14 +0800

    update testmain.py

--- contrib/FireDetection/test/testmain.py
+++ contrib/FireDetection/test/testmain.py
@@ -108,6 +108,8 @@ for item in os.listdir(PATH):
 end = time.time()
 cost_time = end - start
 # Mark spend time
-print("\nImage count: %d\nSpend time: %10.3f\nfps: %10.3f" %(testImg_count, cost_time, testImg_count/cost_time))
+print("Image count:%d" %testImg_count)
+print("Spend time:%10.3f" %cost_time)
+print("fps:%10.3f" %(testImg_count/cost_time))
 # Destroy All Streams
 streamManagerApi.DestroyAllStreams()
