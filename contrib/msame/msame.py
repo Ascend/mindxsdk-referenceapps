3e32bb0eea6edf7ccac399fd5baa43fb04d68bdc
sxiaozhi <1030168088@qq.com>
2022-04-11 20:54:12 +0800

    second commit

--- contrib/msame/msame.py
+++ contrib/msame/msame.py
@@ -1,9 +1,25 @@
-import mindx.sdk as sdk
+# -*- coding: utf-8 -*- 
+# Copyright 2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
 import time
 import numpy as np
 import os
 import sys
 import argparse
+import mindx.sdk as sdk
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='msame-python')
@@ -16,6 +32,7 @@ def parse_args():
 
     args = parser.parse_args()
     return args
+
 
 def make_typemap():
     type_map = {}
@@ -146,7 +163,6 @@ def save_files(filepath,outputs,output,datatype,nums,shape,types_output):
         for i in outputs:
             i.to_host()
             num = np.array(i)
-            #num = np.array(i).astype(type_map[types_output[i_index][0]])
             num = num.flatten()
             num.tofile(output+'/'+filepath.split('.')[0]+'_'+str(i_index)+".bin")
             i_index+=1
