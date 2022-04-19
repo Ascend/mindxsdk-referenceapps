966dfff6106fb3d35746e3014f171bbf77ef0a87
1290045698@qq.com <1290045698@qq.com>
2022-04-19 17:40:48 +0800

    License

--- contrib/VehicleIdentification/main.py
+++ contrib/VehicleIdentification/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-# Copyright(C) 2022. Huawei Technologies Co.,Ltd. All rights reserved.
+# Copyright(C) 2021. Huawei Technologies Co.,Ltd. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
