9995e42213d741a54d009fbe22503dbd0920d66e
张俊涵 <396606075@qq.com>
2022-04-24 03:06:10 +0000

    update contrib/VehicleRetrogradeRecognition/main.cpp.

--- contrib/VehicleRetrogradeRecognition/main.cpp
+++ contrib/VehicleRetrogradeRecognition/main.cpp
@@ -91,7 +91,7 @@ int main(int argc, char* argv[])
         bIsIDR = (output->dataSize > 1);
         if (!m_bFoundFirstIDR)
         {
-            if (!bIsIDR){
+            if (!bIsIDR) {
                 continue;
             } else {
                 m_bFoundFirstIDR = true;
