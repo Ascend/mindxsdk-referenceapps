da4beb72b286c7fa8392fee98068541fcc0dcaf0
lihaowei <distancemay5@gmail.com>
2022-06-02 00:15:32 +0800

    add test

--- contrib/PassengerflowEstimation/main.py
+++ contrib/PassengerflowEstimation/main.py
@@ -2,37 +2,37 @@ import stat
 import json
 import os
 import datetime
-import time
-import signal
 import numpy as np
 import MxpiDataType_pb2 as MxpiDataType
 from StreamManagerApi import StreamManagerApi, MxDataInput, StringVector
 
 if __name__ == '__main__':
+    FLAGS = os.O_RDWR | os.O_APPEND | os.O_CREAT
+    MODES = stat.S_IWUSR | stat.S_IRUSR | stat.S_IRWXU | stat.S_IEXEC
     streamManagerApi = StreamManagerApi()
     ret = streamManagerApi.InitManager()
     if ret != 0:
         print("Failed to init Stream manager, ret=%s" % str(ret))
         exit()
-    with open("/home/dongyu3/fruit/pipelines/passengerflowdetection.pipeline", 'rb') as f:
+    with open("./pipeline/passengerflowestimation.pipeline", 'rb') as f:
         pipelineStr = f.read()
     ret = streamManagerApi.CreateMultipleStreams(pipelineStr)
     if ret != 0:
         print("Failed to create Stream, ret=%s" % str(ret))
         exit()
-    streamName = b'passengerflowestimation_pipline'
-
-    frameid = 0
-    f = open("result.h264", "ab")
-    FRAMENUM = 1200
+    STREAMNAME = b'passengerflowestimation_pipline'
+    # save the result
+    FRAMEID = 0
+    FRAMENUM = 1300
     startTime = time.time()
-    while frameid < FRAMENUM:
-        frameid += 1
-        infer_result = streamManagerApi.GetResult(streamName, 0, 10000)
-        if infer_result.data == b'[1002][Internal error] ':
-            print("Error! cannot find video source!")
-            break
-        f.write(infer_result.data)
+    with os.fdopen(os.open('result.h264', FLAGS, MODES), 'ab+') as f:
+        while FRAMEID < FRAMENUM:
+            FRAMEID += 1
+            infer_result = streamManagerApi.GetResult(STREAMNAME, 0, 10000)
+            if infer_result.data == b'[1002][Internal error] ':
+                print("Error! cannot find video source!")
+                break
+            f.write(infer_result.data)
     endTime = time.time()
     rate = FRAMENUM/(endTime - startTime)
     print("Average_framerate:", rate)
