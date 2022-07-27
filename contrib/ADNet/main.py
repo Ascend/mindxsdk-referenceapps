6551f5a0b236b6118a5bc35242ec71354ef76223
冯子豪 <1223382676@qq.com>
2022-07-27 10:37:49 +0800

    代码提交

--- contrib/ADNet/main.py
+++ contrib/ADNet/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-# Copyright 2021 Huawei Technologies Co., Ltd
+# Copyright 2022 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
