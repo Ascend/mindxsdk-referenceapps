b86934a848adee3f144fb7cdf3b416a210c46aa4
梁秀波 <xiubo@zju.edu.cn>
2022-09-13 06:34:23 +0000

    update contrib/OpenCVPlugin/build.sh.
    Signed-off-by: 梁秀波 <xiubo@zju.edu.cn>

--- contrib/OpenCVPlugin/build.sh
+++ contrib/OpenCVPlugin/build.sh
@@ -2,5 +2,5 @@ mkdir OpenCVPlugin/src/OpenCVPlugin/build
 cd OpenCVPlugin/src/OpenCVPlugin/build
 cmake ..
 make -j4
-chmod 400 ../lib/plugins/libOpenCVPlugin.so
-cp ../lib/plugins/libOpenCVPlugin.so ${MX_SDK_HOME}/lib/plugins
+chmod 400 ../lib/plugins/libmxpi_OpenCVPlugin.so
+cp ../lib/plugins/libmxpi_OpenCVPlugin.so ${MX_SDK_HOME}/lib/plugins
