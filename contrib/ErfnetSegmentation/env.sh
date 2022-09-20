f5e936ccddadc5775ac0d03b521d5295fd5532ea
gaopeifeng <1628935449@qq.com>
2022-09-20 13:51:32 +0800

    .

--- contrib/ErfnetSegmentation/env.sh
+++ contrib/ErfnetSegmentation/env.sh
@@ -1,4 +1,3 @@
-#! /bin/bash
 export install_path=/usr/local/Ascend/ascend-toolkit/latest
 export PATH=/usr/local/python3.7.5/bin:${install_path}/atc/ccec_compiler/bin:${install_path}/atc/bin:$PATH
 export PYTHONPATH=${install_path}/atc/python/site-packages:$PYTHONPATH
