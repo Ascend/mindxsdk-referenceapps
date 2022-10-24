a2e9c2b3431d0c4f34bddfb69dfffafadc868319
张雪晴 <849975615@qq.com>
2022-10-24 08:30:06 +0000

    infer
    Signed-off-by: 张雪晴 <849975615@qq.com>

---
+++ contrib/TSM/jester.py
@@ -0,0 +1,182 @@
+# Copyright(C) 2022. Huawei Technologies Co.,Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import os
+import io
+import time
+from typing import Tuple
+from PIL import Image, ImageOps
+import mindx.sdk as sdk
+import subprocess
+import threading
+import shutil
+from multiprocessing import Manager
+from multiprocessing import Process
+
+SOFTMAX_THRES = 0
+HISTORY_LOGIT = True
+REFINE_OUTPUT = True
+filepath = "./model/jester.om"
+device_id = 0
+
+if not os.path.exists('./image'):
+    os.makedirs('./image')
+else:
+    shutil.rmtree('./image')
+    os.makedirs('./image')   
+    
+catigories = [
+    "Doing other things",  # 0
+    "Drumming Fingers",  # 1
+    "No gesture",  # 2
+    "Pulling Hand In",  # 3
+    "Pulling Two Fingers In",  # 4
+    "Pushing Hand Away",  # 5
+    "Pushing Two Fingers Away",  # 6
+    "Rolling Hand Backward",  # 7
+    "Rolling Hand Forward",  # 8
+    "Shaking Hand",  # 9
+    "Sliding Two Fingers Down",  # 10
+    "Sliding Two Fingers Left",  # 11
+    "Sliding Two Fingers Right",  # 12
+    "Sliding Two Fingers Up",  # 13
+    "Stop Sign",  # 14
+    "Swiping Down",  # 15
+    "Swiping Left",  # 16
+    "Swiping Right",  # 17
+    "Swiping Up",  # 18
+    "Thumb Down",  # 19
+    "Thumb Up",  # 20
+    "Turning Hand Clockwise",  # 21
+    "Turning Hand Counterclockwise",  # 22
+    "Zooming In With Full Hand",  # 23
+    "Zooming In With Two Fingers",  # 24
+    "Zooming Out With Full Hand",  # 25
+    "Zooming Out With Two Fingers"  # 26
+]
+
+
+n_still_frame = 0
+
+def process_output(idx_, history):
+    # idx_: the output of current frame
+    # history: a list containing the history of predictions
+    if not REFINE_OUTPUT:
+        return idx_, history
+
+    max_hist_len = 20  # max history buffer
+
+    # mask out illegal action
+    if idx_ in [7, 8, 21, 22, 3]:
+        idx_ = history[-1]
+
+    # use only single no action class
+    if idx_ == 0:
+        idx_ = 2
+    
+    # history smoothing
+    if idx_ != history[-1]:
+        if not (history[-1] == history[-2]): #  and history[-2] == history[-3]):
+            idx_ = history[-1]
+    
+
+    history.append(idx_)
+    history = history[-max_hist_len:]
+
+    return history[-1], history
+
+imgs = []
+
+def video2img():
+    cmd = 'ffmpeg  -i \"{}\" -threads 1 -vf scale=-1:331 -q:v 0 \"{}/img_%05d.jpg\"'.format('rtsp://192.168.88.110:1240/jester.264', './image')
+    subprocess.call(cmd, shell=True,
+                    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+def readimg():
+    global imgs
+    while True:
+        for filename in os.listdir(r"./image"):
+            if filename not in imgs:
+                imgs.append(filename)
+def crop_image(re_img,new_height,new_width):
+    re_img=Image.fromarray(np.uint8(re_img))
+    width, height = re_img.size
+    left = (width - new_width)/2
+    top = (height - new_height)/2
+    right = (width + new_width)/2
+    bottom = (height + new_height)/2
+    crop_im = re_img.crop((left, top, right, bottom))
+    crop_im = np.asarray(crop_im)
+    return crop_im
+
+def main():
+    index = 0
+    time.sleep(10)
+    buffer = [
+        sdk.Tensor(np.zeros([1, 3, 56, 56],dtype=np.float32)),
+        sdk.Tensor(np.zeros([1, 4, 28, 28],dtype=np.float32)),
+        sdk.Tensor(np.zeros([1, 4, 28, 28],dtype=np.float32)),
+        sdk.Tensor(np.zeros([1, 8, 14, 14],dtype=np.float32)),
+        sdk.Tensor(np.zeros([1, 8, 14, 14],dtype=np.float32)),
+        sdk.Tensor(np.zeros([1, 8, 14, 14],dtype=np.float32)),
+        sdk.Tensor(np.zeros([1, 12, 14, 14],dtype=np.float32)),
+        sdk.Tensor(np.zeros([1, 12, 14, 14],dtype=np.float32)),
+        sdk.Tensor(np.zeros([1, 20, 7, 7],dtype=np.float32)),
+        sdk.Tensor(np.zeros([1, 20, 7, 7],dtype=np.float32))]
+    for t in buffer:
+        t.to_device(device_id)
+    md = sdk.model(filepath, device_id) 
+    i_frame = -2
+    history = [2]
+    history_logit = []
+    history_timing = []
+    while True:
+        i_frame += 2
+        time.sleep(0.2)        
+        filename = imgs[i_frame]
+        img = Image.open("./image/" + filename).convert('RGB')
+        if img.width>img.height:
+            frame_pil = img.resize((round(256*img.width/img.height),256))
+        else:
+            frame_pil = img.resize((256,round(256*img.height/img.width)))
+        image = crop_image(frame_pil,224,224).transpose(2,0,1)
+        img_tran = [0,0,0]
+        for i in range(3):
+            img_tran[0]=(image[0]/255-0.485)/0.229
+            img_tran[1]=(image[1]/255-0.456)/0.224
+            img_tran[2]=(image[2]/255-0.406)/0.225
+        img_tran = np.array(img_tran).astype(np.float32)
+        img_tran = sdk.Tensor(img_tran)
+        img_tran.to_device(device_id)
+        inputs = [img_tran,] + buffer
+        outputs = md.infer(inputs)
+        buffer = outputs[1:]
+        outputs[0].to_host()
+        out = outputs[0]  
+        feat = np.array(out)
+        idx_ = np.argmax(feat, axis=1)[0]
+        history_logit.append(feat)
+        history_logit = history_logit[-12:]
+        avg_logit = sum(history_logit)
+        idx_ = np.argmax(avg_logit, axis=1)[0]            
+        idx, history = process_output(idx_, history)
+
+        print(f"{index} {catigories[idx]}")
+        index += 1
+s1 = threading.Thread(target=video2img)
+s2 = threading.Thread(target=readimg)
+if __name__ == '__main__':
+    s1.start()
+    s2.start()
+    main()
---
+++ contrib/TSM/offline_infer.py
@@ -0,0 +1,257 @@
+# Copyright(C) 2022. Huawei Technologies Co.,Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import os
+import time
+import torch.nn.parallel
+import torch.optim
+import numpy as np
+from ops.dataset import TSNDataSet
+from ops.transforms import *
+from ops import dataset_config
+from torch.nn import functional as F
+from sklearn.metrics import confusion_matrix
+import mindx.sdk as sdk
+
+# options
+parser = argparse.ArgumentParser(description="TSM testing on the full validation set")
+parser.add_argument('dataset', type=str)
+
+# may contain splits
+parser.add_argument('--weights', type=str, default='TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e50.pth')
+parser.add_argument('--test_segments', type=str, default=25)
+parser.add_argument('--dense_sample', default=False, action="store_true", help='use dense sample as I3D')
+parser.add_argument('--twice_sample', default=False, action="store_true", help='use twice sample for ensemble')
+parser.add_argument('--full_res', default=False, action="store_true",
+                    help='use full resolution 256x256 for test as in Non-local I3D')
+
+parser.add_argument('--test_crops', type=int, default=1)
+parser.add_argument('--coeff', type=str, default=None)
+parser.add_argument('--batch_size', type=int, default=1)
+parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
+                    help='number of data loading workers (default: 8)')
+
+# for true test
+parser.add_argument('--test_list', type=str, default=None)
+parser.add_argument('--csv_file', type=str, default=None)
+
+parser.add_argument('--softmax', default=False, action="store_true", help='use softmax')
+
+parser.add_argument('--max_num', type=int, default=-1)
+parser.add_argument('--input_size', type=int, default=224)
+parser.add_argument('--crop_fusion_type', type=str, default='avg')
+parser.add_argument('--gpus', nargs='+', type=int, default=None)
+parser.add_argument('--img_feature_dim',type=int, default=256)
+parser.add_argument('--num_set_segments',type=int, default=1,help='TODO: select multiply set of n-frames from a video')
+parser.add_argument('--pretrain', type=str, default='imagenet')
+
+args = parser.parse_args()
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    res = []
+    for k in topk:
+         correct_k = correct[:k].view(-1).float().sum(0)
+         res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+def parse_shift_option_from_log_name(log_name):
+    if 'shift' in log_name:
+        strings = log_name.split('_')
+        for i, s in enumerate(strings):
+            if 'shift' in s:
+                break
+        return True, int(strings[i].replace('shift', '')), strings[i + 1]
+    else:
+        return False, None, None
+
+
+weights_list = args.weights.split(',')
+test_segments_list = [int(s) for s in args.test_segments.split(',')]
+assert len(weights_list) == len(test_segments_list)
+if args.coeff is None:
+    coeff_list = [1] * len(weights_list)
+else:
+    coeff_list = [float(c) for c in args.coeff.split(',')]
+
+if args.test_list is not None:
+    test_file_list = args.test_list.split(',')
+else:
+    test_file_list = [None] * len(weights_list)
+
+
+data_iter_list = []
+net_list = []
+modality_list = []
+
+total_num = None
+for this_weights, this_test_segments, test_file in zip(weights_list, test_segments_list, test_file_list):
+    is_shift, shift_div, shift_place = parse_shift_option_from_log_name(this_weights)
+    if 'RGB' in this_weights:
+        modality = 'RGB'
+    else:
+        modality = 'Flow'
+    this_arch = this_weights.split('TSM_')[1].split('_')[2]
+    modality_list.append(modality)
+    num_class, args.train_list, val_list, root_path, prefix = dataset_config.return_dataset(args.dataset, modality)
+    
+    input_size = 224
+    cropping = torchvision.transforms.Compose([
+            GroupScale(256),
+            GroupCenterCrop(input_size),
+        ])    
+    
+    data_loader = torch.utils.data.DataLoader(
+            TSNDataSet(root_path, test_file if test_file is not None else val_list, num_segments=this_test_segments,
+                       new_length=1 if modality == "RGB" else 5,
+                       modality=modality,
+                       image_tmpl=prefix,
+                       test_mode=True,
+                       remove_missing = len(weights_list) == 1,
+                       transform=torchvision.transforms.Compose([
+                           cropping,
+                           Stack(roll=(this_arch in ['BNInception', 'InceptionV3'])),
+                           ToTorchFormatTensor(div=(this_arch not in ['BNInception', 'InceptionV3'])),
+                           GroupNormalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                       ]), dense_sample=args.dense_sample, twice_sample=args.twice_sample),
+            batch_size=args.batch_size, shuffle=False,
+            num_workers=args.workers, pin_memory=True,
+    )
+
+
+    if args.gpus is not None:
+        devices = [args.gpus[i] for i in range(args.workers)]
+    else:
+        devices = list(range(args.workers))
+
+    data_gen = enumerate(data_loader)
+
+    if total_num is None:
+        total_num = len(data_loader.dataset)
+    else:
+        assert total_num == len(data_loader.dataset)
+
+    data_iter_list.append(data_gen)
+
+def eval_video(video_data, this_test_segments, modality):
+    with torch.no_grad():
+        i, data, label = video_data
+        batch_size = label.numel()
+        num_crop = args.test_crops
+        if args.dense_sample:
+            num_crop *= 10  # 10 clips for testing when using dense sample
+
+        if args.twice_sample:
+            num_crop *= 2
+
+        if modality == 'RGB':
+            length = 3
+        elif modality == 'Flow':
+            length = 10
+        elif modality == 'RGBDiff':
+            length = 18
+        else:
+            raise ValueError("Unknown modality "+ modality)
+
+        data_in = data.view(-1, length, data.size(2), data.size(3))
+        if is_shift:
+            data_in = data_in.view(batch_size * num_crop, this_test_segments, length, data_in.size(2), data_in.size(3))
+        filepath = "./model/TSM.om"
+        device_id = 0                          
+        m = sdk.model(filepath, device_id)
+        t = sdk.Tensor(np.array(data_in))
+        t.to_device(0)
+        rst = m.infer(t)
+        rst[0].to_host()
+        rst = rst[0]
+        rst = np.array(rst)
+        rst = rst.reshape(batch_size, num_crop, -1).mean(1)
+        print(data_in)
+        print(rst)
+        if args.softmax:
+            # take the softmax to normalize the output to probability
+            rst = F.softmax(rst, dim=1)
+        rst = torch.Tensor(rst)
+        rst = rst.data.cpu().numpy().copy()
+
+        if is_shift:
+            rst = rst.reshape(batch_size, num_class)
+        else:
+            rst = rst.reshape((batch_size, -1, num_class)).mean(axis=1).reshape((batch_size, num_class))
+
+        return i, rst, label
+
+output = []
+proc_start_time = time.time()
+max_num = args.max_num if args.max_num > 0 else total_num
+
+top1 = AverageMeter()
+top5 = AverageMeter()
+
+"""
+read pipeline and do infer
+"""
+
+for i, data_label_pairs in enumerate(zip(*data_iter_list)):
+    with torch.no_grad():
+        if i >= max_num:
+            break
+        this_rst_list = []
+        this_label = None
+        for n_seg, (_, (data, label)), modality in zip(test_segments_list, data_label_pairs, modality_list):
+            rst = eval_video((i, data, label), n_seg, modality)
+            this_rst_list.append(rst[1])
+            this_label = label
+        assert len(this_rst_list) == len(coeff_list)
+        for i_coeff in range(len(this_rst_list)):
+            this_rst_list[i_coeff] *= coeff_list[i_coeff]
+        ensembled_predict = sum(this_rst_list) / len(this_rst_list)
+
+        for p, g in zip(ensembled_predict, this_label.cpu().numpy()):
+            output.append([p[None, ...], g])
+        cnt_time = time.time() - proc_start_time
+        prec1, prec5 = accuracy(torch.from_numpy(ensembled_predict), this_label, topk=(1, 5))
+        top1.update(prec1.item(), this_label.numel())
+        top5.update(prec5.item(), this_label.numel())
+        print('video {} done, total {}/{}, average {:.3f} sec/video, '
+              'moving Prec@1 {:.3f} Prec@5 {:.3f}'.format(i * args.batch_size, i * args.batch_size, total_num,
+                                                        float(cnt_time) / (i+1), top1.avg, top5.avg))
+
+print('-----Evaluation is finished------')
+print('Overall Prec@1 {:.02f}% Prec@5 {:.02f}%'.format(top1.avg, top5.avg))
