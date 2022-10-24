2848e893a5c326237a74448d54083fed191906ab
张雪晴 <849975615@qq.com>
2022-10-24 08:30:39 +0000

    ops
    Signed-off-by: 张雪晴 <849975615@qq.com>

---
+++ contrib/TSM/ops/basic_ops.py
@@ -0,0 +1,51 @@
+# Copyright(C) 2022. Huawei Technologies Co.,Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+
+
+class Identity(torch.nn.Module):
+    def forward(self, input):
+        return input
+
+
+class SegmentConsensus(torch.nn.Module):
+
+    def __init__(self, consensus_type, dim=1):
+        super(SegmentConsensus, self).__init__()
+        self.consensus_type = consensus_type
+        self.dim = dim
+        self.shape = None
+
+    def forward(self, input_tensor):
+        self.shape = input_tensor.size()
+        if self.consensus_type == 'avg':
+            output = input_tensor.mean(dim=self.dim, keepdim=True)
+        elif self.consensus_type == 'identity':
+            output = input_tensor
+        else:
+            output = None
+
+        return output
+
+
+class ConsensusModule(torch.nn.Module):
+
+    def __init__(self, consensus_type, dim=1):
+        super(ConsensusModule, self).__init__()
+        self.consensus_type = consensus_type if consensus_type != 'rnn' else 'identity'
+        self.dim = dim
+
+    def forward(self, input):
+        return SegmentConsensus(self.consensus_type, self.dim)(input)
