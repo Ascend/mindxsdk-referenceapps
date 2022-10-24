0a095ee597d9bb48c67084d3a0e395c085a2e11f
张雪晴 <849975615@qq.com>
2022-10-24 08:32:02 +0000

    model
    Signed-off-by: 张雪晴 <849975615@qq.com>

---
+++ contrib/TSM/model/pth2onnx.py
@@ -0,0 +1,51 @@
+# Copyright(C) 2022. Huawei Technologies Co.,Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import torch.onnx
+import torch.nn.parallel
+from ops.models import TSN
+
+def pth_to_onnx(input_shape, checkpoint, onnx_path, input_names=['video'], output_names=['class']):
+    if not onnx_path.endswith('.onnx'):
+        print('Warning! The onnx model name is not correct,\
+              please give a name that ends with \'.onnx\'!')
+        return 0
+    net = TSN(400, 8, 'RGB',
+                base_model='resnet50',
+                consensus_type='avg' ,
+                img_feature_dim=256,
+                pretrain='imagenet',
+                is_shift=True, shift_div=8, shift_place='blockres',
+                non_local=False,)
+    checkpoint = torch.load(checkpoint)
+    checkpoint = checkpoint['state_dict']
+    base_dict = {'.'.join(k.split('.')[1:]): v for k, v in list(checkpoint.items())}
+    replace_dict = {'base_model.classifier.weight': 'new_fc.weight',
+                    'base_model.classifier.bias': 'new_fc.bias',
+                    }
+    for k, v in replace_dict.items():
+        if k in base_dict:
+            base_dict[v] = base_dict.pop(k)
+    net.load_state_dict(base_dict)
+    net.eval()
+    torch.onnx.export(net, input_shape, onnx_path, input_names=input_names, output_names=output_names, opset_version=11)
+    print("Exporting .pth model to onnx model has been successful!")
+
+if __name__ == '__main__':
+    pth = './TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e50.pth'
+    onnx_path = './TSM.onnx'
+    input_shape = torch.randn(1, 8, 3, 224, 224)
+    pth_to_onnx(input_shape, pth, onnx_path)
+
