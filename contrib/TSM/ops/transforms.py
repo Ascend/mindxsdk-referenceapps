802dacfd30a23bd645645e6f96806c53de2f90bf
张雪晴 <849975615@qq.com>
2022-10-25 17:09:18 +0000

    update contrib/TSM/ops/transforms.py.
    Signed-off-by: 张雪晴 <849975615@qq.com>

--- contrib/TSM/ops/transforms.py
+++ contrib/TSM/ops/transforms.py
@@ -12,12 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import torchvision
+import math
 import random
+import numbers
+import torchvision
 from PIL import Image, ImageOps
 import numpy as np
-import numbers
-import math
 import torch
 
 
