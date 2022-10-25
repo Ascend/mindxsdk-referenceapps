0efc97a437b4f826e8f0b6f5a7fe2b7842c2ef2e
zangyaning <ethanhaha2@163.com>
2022-10-25 04:09:23 +0000

    update
    Signed-off-by: zangyaning <ethanhaha2@163.com>

--- contrib/SuperRetina/onnx2om.sh
+++ contrib/SuperRetina/onnx2om.sh
@@ -27,12 +27,7 @@ om_path=$2
 
 echo "Input ONNX file path: ${onnx_path}"
 echo "Output OM file path: ${om_path}"
-export install_path=/usr/local/Ascend/ascend-toolkit/latest
-export PATH=/usr/local/python3.7.5/bin:${install_path}/atc/ccec_compiler/bin:${install_path}/atc/bin:$PATH
-export PYTHONPATH=${install_path}/atc/python/site-packages:$PYTHONPATH
-export LD_LIBRARY_PATH=${install_path}/atc/lib64:${install_path}/acllib/lib64:$LD_LIBRARY_PATH
-export ASCEND_OPP_PATH=${install_path}/opp
-export ASCEND_AICPU_PATH=${install_path}
+
 atc --framework=5 --model="${onnx_path}" \
     --output="${om_path}" \
     --soc_version=Ascend310 \
