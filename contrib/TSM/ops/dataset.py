9aa0654883617cdb7d38c9961d232a6db41dde3b
张雪晴 <849975615@qq.com>
2022-10-26 15:36:52 +0000

    update contrib/TSM/ops/dataset.py.
    Signed-off-by: 张雪晴 <849975615@qq.com>

--- contrib/TSM/ops/dataset.py
+++ contrib/TSM/ops/dataset.py
@@ -134,21 +134,6 @@ class TSNDataSet(data.Dataset):
                 offsets = np.zeros((self.num_segments,))
             return offsets + 1
 
-    def _get_val_indices(self, record):
-        if self.dense_sample:  # i3d dense sample
-            sample_pos = max(1, 1 + record.num_frames - 64)
-            t_stride = 64 // self.num_segments
-            start_idx = 0 if sample_pos == 1 else np.random.randint(0, sample_pos - 1)
-            offsets = [(idx * t_stride + start_idx) % record.num_frames for idx in range(self.num_segments)]
-            return np.array(offsets) + 1
-        else:
-            if record.num_frames > self.num_segments + self.new_length - 1:
-                tick = (record.num_frames - self.new_length + 1) / float(self.num_segments)
-                offsets = np.array([int(tick / 2.0 + tick * x) for x in range(self.num_segments)])
-            else:
-                offsets = np.zeros((self.num_segments,))
-            return offsets + 1
-
     def __getitem__(self, index):
         record = self.video_list[index]
         # check this is a legit video folder
@@ -182,6 +167,25 @@ class TSNDataSet(data.Dataset):
         else:
             segment_indices = self._get_test_indices(record)
         return self.get(record, segment_indices)
+    
+    def _get_val_indices(self, record):
+        if self.dense_sample:  # i3d dense sample
+            sample_pos = max(1, 1 + record.num_frames - 64)
+            t_stride = 64 // self.num_segments
+            start_idx = 0 if sample_pos == 1 else np.random.randint(0, sample_pos - 1)
+            offsets = [(idx * t_stride + start_idx) % record.num_frames for idx in range(self.num_segments)]
+            return np.array(offsets) + 1
+        else:
+            if record.num_frames > self.num_segments + self.new_length - 1:
+                tick = (record.num_frames - self.new_length + 1) / float(self.num_segments)
+                offsets = np.array([int(tick / 2.0 + tick * x) for x in range(self.num_segments)])
+            else:
+                offsets = np.zeros((self.num_segments,))
+            return offsets + 1
+    
+    def __len__(self):
+        return len(self.video_list)
+
     def _get_test_indices(self, record):
         if self.dense_sample:
             sample_pos = max(1, 1 + record.num_frames - 64)
@@ -202,9 +206,6 @@ class TSNDataSet(data.Dataset):
             tick = (record.num_frames - self.new_length + 1) / float(self.num_segments)
             offsets = np.array([int(tick / 2.0 + tick * x) for x in range(self.num_segments)])
             return offsets + 1
-
-    def __len__(self):
-        return len(self.video_list)
     
     def get(self, record, indices):
         images = list()
