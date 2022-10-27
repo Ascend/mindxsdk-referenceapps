c2dfd56f06712560065e5fe187324c6940641e24
张雪晴 <849975615@qq.com>
2022-10-27 07:10:37 +0000

    update contrib/TSM/ops/transforms.py.
    Signed-off-by: 张雪晴 <849975615@qq.com>

--- contrib/TSM/ops/transforms.py
+++ contrib/TSM/ops/transforms.py
@@ -105,45 +105,6 @@ class GroupScale(object):
         return [self.worker(img) for img in img_group]
 
 
-class GroupOverSample(object):
-    def __init__(self, crop_size, scale_size=None, flip=True):
-        self.crop_size = crop_size if not isinstance(crop_size, int) else (crop_size, crop_size)
-
-        if scale_size is not None:
-            self.scale_worker = GroupScale(scale_size)
-        else:
-            self.scale_worker = None
-        self.flip = flip
-
-    def __call__(self, img_group):
-
-        if self.scale_worker is not None:
-            img_group = self.scale_worker(img_group)
-
-        image_w, image_h = img_group[0].size
-        crop_w, crop_h = self.crop_size
-
-        offsets = GroupMultiScaleCrop.fill_fix_offset(False, image_w, image_h, crop_w, crop_h)
-        oversample_group = list()
-        for o_w, o_h in offsets:
-            normal_group = list()
-            flip_group = list()
-            for i, img in enumerate(img_group):
-                crop = img.crop((o_w, o_h, o_w + crop_w, o_h + crop_h))
-                normal_group.append(crop)
-                flip_crop = crop.copy().transpose(Image.FLIP_LEFT_RIGHT)
-
-                if img.mode == 'L' and i % 2 == 0:
-                    flip_group.append(ImageOps.invert(flip_crop))
-                else:
-                    flip_group.append(flip_crop)
-
-            oversample_group.extend(normal_group)
-            if self.flip:
-                oversample_group.extend(flip_group)
-        return oversample_group
-
-
 class GroupFullResSample(object):
     def __init__(self, crop_size, scale_size=None, flip=True):
         self.crop_size = crop_size if not isinstance(crop_size, int) else (crop_size, crop_size)
@@ -188,80 +149,6 @@ class GroupFullResSample(object):
             oversample_group.extend(normal_group)
             oversample_group.extend(flip_group)
         return oversample_group
-
-
-class GroupMultiScaleCrop(object):
-
-    def __init__(self, input_size, scales=None, max_distort=1, fix_crop=True, more_fix_crop=True):
-        self.scales = scales if scales is not None else [1, .875, .75, .66]
-        self.max_distort = max_distort
-        self.fix_crop = fix_crop
-        self.more_fix_crop = more_fix_crop
-        self.input_size = input_size if not isinstance(input_size, int) else [input_size, input_size]
-        self.interpolation = Image.BILINEAR
-
-    def __call__(self, img_group):
-
-        im_size = img_group[0].size
-
-        crop_w, crop_h, offset_w, offset_h = self._sample_crop_size(im_size)
-        crop_img_group = [img.crop((offset_w, offset_h, offset_w + crop_w, offset_h + crop_h)) for img in img_group]
-        ret_img_group = [img.resize((self.input_size[0], self.input_size[1]), self.interpolation)
-                         for img in crop_img_group]
-        return ret_img_group
-
-    @staticmethod
-    def fill_fix_offset(more_fix_crop, image_w, image_h, crop_w, crop_h):
-        w_step = (image_w - crop_w) // 4
-        h_step = (image_h - crop_h) // 4
-
-        ret = list()
-        ret.append((0, 0))  # upper left
-        ret.append((4 * w_step, 0))  # upper right
-        ret.append((0, 4 * h_step))  # lower left
-        ret.append((4 * w_step, 4 * h_step))  # lower right
-        ret.append((2 * w_step, 2 * h_step))  # center
-
-        if more_fix_crop:
-            ret.append((0, 2 * h_step))  # center left
-            ret.append((4 * w_step, 2 * h_step))  # center right
-            ret.append((2 * w_step, 4 * h_step))  # lower center
-            ret.append((2 * w_step, 0 * h_step))  # upper center
-
-            ret.append((1 * w_step, 1 * h_step))  # upper left quarter
-            ret.append((3 * w_step, 1 * h_step))  # upper right quarter
-            ret.append((1 * w_step, 3 * h_step))  # lower left quarter
-            ret.append((3 * w_step, 3 * h_step))  # lower righ quarter
-
-        return ret
-
-    def _sample_crop_size(self, im_size):
-        image_w, image_h = im_size[0], im_size[1]
-
-        # find a crop size
-        base_size = min(image_w, image_h)
-        crop_sizes = [int(base_size * x) for x in self.scales]
-        crop_h = [self.input_size[1] if abs(x - self.input_size[1]) < 3 else x for x in crop_sizes]
-        crop_w = [self.input_size[0] if abs(x - self.input_size[0]) < 3 else x for x in crop_sizes]
-
-        pairs = []
-        for i, h in enumerate(crop_h):
-            for j, w in enumerate(crop_w):
-                if abs(i - j) <= self.max_distort:
-                    pairs.append((w, h))
-
-        crop_pair = random.choice(pairs)
-        if not self.fix_crop:
-            w_offset = random.randint(0, image_w - crop_pair[0])
-            h_offset = random.randint(0, image_h - crop_pair[1])
-        else:
-            w_offset, h_offset = self._sample_fix_offset(image_w, image_h, crop_pair[0], crop_pair[1])
-
-        return crop_pair[0], crop_pair[1], w_offset, h_offset
-
-    def _sample_fix_offset(self, image_w, image_h, crop_w, crop_h):
-        offsets = self.fill_fix_offset(self.more_fix_crop, image_w, image_h, crop_w, crop_h)
-        return random.choice(offsets)
 
 
 class GroupRandomSizedCrop(object):
