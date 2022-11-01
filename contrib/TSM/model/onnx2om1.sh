a4e7592be7be89b88f09b9d16b29153f0a8709b1
张雪晴 <849975615@qq.com>
2022-11-01 12:59:24 +0000

    update contrib/TSM/model/onnx2om1.sh.
    Signed-off-by: 张雪晴 <849975615@qq.com>

--- contrib/TSM/model/onnx2om1.sh
+++ contrib/TSM/model/onnx2om1.sh
@@ -13,13 +13,6 @@
 # limitations under the License.
 #!/bin/bash
 
-export install_path=/usr/local/Ascend/ascend-toolkit/latest
-export PATH=/usr/local/python3.7.5/bin:${install_path}/atc/ccec_compiler/bin:${install_path}/atc/bin:$PATH
-export PYTHONPATH=${install_path}/atc/python/site-packages:$PYTHONPATH
-export LD_LIBRARY_PATH=${install_path}/atc/lib64:${install_path}/acllib/lib64:$LD_LIBRARY_PATH
-export ASCEND_OPP_PATH=${install_path}/opp
-export ASCEND_AICPU_PATH=${install_path}
-
 atc --model=./jester.onnx --framework=5 --output=./jester --input_format=NCDHW  --soc_version=Ascend310  --precision_mode=allow_fp32_to_fp16 --op_select_implmode=high_precision
 
 if [ -f "./jester.om" ]; then
