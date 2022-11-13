fd2ce4ff7580acb019aa15f900cb618f3bf45490
Wangyijia123 <674858380@qq.com>
2022-11-13 10:18:18 +0800

    delete

--- contrib/TextSnake/evaluate.py
+++ contrib/TextSnake/evaluate.py
@@ -16,8 +16,6 @@
 # limitations under the License.
 
 
-#思路：和main一样的先进行预处理再输入输出，只不过多次循环 每次循环的输出写到一个文件里，文件名是输入图像名.txt,最后调用deteval对这个结果文件夹里的文件进行计算精度
-
 import json
 import os
 import math
@@ -27,17 +25,29 @@ import numpy as np
 import MxpiDataType_pb2 as MxpiDataType
 from PIL import Image
 import torch
-# from StreamManagerApi import StreamManagerApi, MxDataInput, StringVector
+
 from StreamManagerApi import StreamManagerApi, MxProtobufIn, InProtobufVector, StringVector
 
-from misc import fill_hole, regularize_sin_cos
-from textdetector import TextDetector
-from misc import to_device, mkdirs, rescale_result
-from config import config as cfg
-from visualize import visualize_detection
-from imagereader import Resize,Normalize
+from util.misc import fill_hole, regularize_sin_cos
+from util.detection import TextDetector
+from util.misc import to_device, mkdirs, rescale_result
+from util.config import config as cfg
+from util.visualize import visualize_detection
 import subprocess
-# np.set_printoptions(threshold=np.inf)
+
+def norm(image,mean,std):
+    image = image.astype(np.float32)
+    image /= 255.0
+    image -= mean
+    image /= std
+    return image
+
+def resize(image,size):
+    h, w, _ = image.shape
+    image = cv2.resize(image, (size,size))
+    scales = np.array([size / w, size / h])
+    return image
+
 def write_to_file(contours, file_path):
     with open(file_path, 'w') as f:
         for cont in contours:
@@ -47,33 +57,14 @@ def write_to_file(contours, file_path):
             f.write(cont + '\n')
 
 if __name__ == '__main__':
-    # streamManagerApi = StreamManagerApi()
-    # 新建一个流管理StreamManager对象并初始化
-    # ret = streamManagerApi.InitManager()
-    # if ret != 0:
-    #     print("Failed to init Stream manager, ret=%s" % str(ret))
-    #     exit()
-
-    # 构建pipeline
-    # PIPELINE_PATH = "./t.pipeline"
-    # if os.path.exists(PIPELINE_PATH) != 1:
-    #     print("pipeline does not exist !")
-    #     exit()
-    # with open(PIPELINE_PATH, 'rb') as f:
-    #     pipelineStr = f.read()
-    #     ret = streamManagerApi.CreateMultipleStreams(pipelineStr)
-    #     if ret != 0:
-    #         print("Failed to create Stream, ret=%s" % str(ret))
-    #         exit()
-
     steam_manager_api = StreamManagerApi()
-    # init stream manager
+    
     ret = steam_manager_api.InitManager()
     if ret != 0:
         print("Failed to init Stream manager, ret=%s" % str(ret))
         exit()
-    # input_shape = [416, 416]
-    # create streams by pipeline config file
+    
+    
     MODES = stat.S_IWUSR | stat.S_IRUSR
     with os.fdopen(os.open("./t.pipeline", os.O_RDONLY, MODES), 'rb') as f:
         pipeline_str = f.read()
@@ -82,8 +73,9 @@ if __name__ == '__main__':
         print("Failed to create Stream, ret=%s" % str(ret))
         exit()
 
-    # 构建流的输入对象--检测目标
-    # dataInput = MxDataInput()
+    
+    means = (0.485, 0.456, 0.406)
+    stds = (0.229, 0.224, 0.225)
     FILEPATH = "./data/total-text/"
     if os.path.exists(FILEPATH) != 1:
         print("The filepath does not exist !")
@@ -100,23 +92,10 @@ if __name__ == '__main__':
         image = Image.open(IMAGE_PATH)
         image = np.array(image)
         H, W, _ = image.shape
-        rsz=Resize(cfg.input_size)
-        image,_=rsz(image)
-        norm=Normalize(cfg.means,cfg.stds)
-        image,_=norm(image)
+        image=resize(image,cfg.input_size)
+        image=norm(image,np.array(means),np.array(stds))
         image=image.transpose(2, 0, 1)
         print(IMAGE_PATH1)
-    #     cv2.imwrite(IMAGE_PATH1, image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-    #     # binpath=IMAGE_PATH1.replace('jpg', 'bin')
-    #     # image.tofile(binpath)
-    #     # print(binpath)
-    #     with open(IMAGE_PATH1, 'rb') as f:
-    #         dataInput.data = f.read()
-    #         begin_array = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    #         STREAMNAME = b'detection'
-    #         INPLUGINID = 0
-    # # 根据流名将检测目标传入流中
-    #     uniqueId = streamManagerApi.SendData(STREAMNAME, INPLUGINID, dataInput)
         visionList = MxpiDataType.MxpiVisionList()
         visionVec = visionList.visionVec.add()
 
@@ -135,8 +114,6 @@ if __name__ == '__main__':
         KEY0 = b"appsrc0"
 
         protobufVec = InProtobufVector()
-
-
         protobuf = MxProtobufIn()
         protobuf.key = KEY0
         protobuf.type = b"MxTools.MxpiVisionList"
@@ -152,8 +129,8 @@ if __name__ == '__main__':
         keyVec = StringVector()
         for key in keys:
             keyVec.push_back(key)
-    # 从流中取出对应插件的输出数据
-    #     infer = streamManagerApi.GetResult(STREAMNAME, b'appsink0', keyVec)
+    
+    
         infer = steam_manager_api.GetResult(STEAMNAME, b'appsink0', keyVec)
         if(infer.metadataVec.size() == 0):
             print("Get no data from stream !")
@@ -171,28 +148,10 @@ if __name__ == '__main__':
         sin_pred=pred_array[:,4,:,:].reshape(512,512)
         cos_pred=pred_array[:,5,:,:].reshape(512,512)
         radii_pred=pred_array[:,6,:,:].reshape(512,512)
-        # tr_pred = pred[0:524288]#0-524287
-        # tcl_pred = pred[524288:1048576]#524287-1048575
-        # sin_pred = pred[1048576:1310720]
-        # cos_pred = pred[1310720:1572864]
-        # radii_pred = pred[1572864:1835008]
-        # tr_pred= np.array(tr_pred).reshape(2,512,512)
-        # tcl_pred= np.array(tcl_pred).reshape(2,512,512)
-        # sin_pred= np.array(sin_pred).reshape(512,512)
-        # cos_pred= np.array(cos_pred).reshape(512,512)
-        # radii_pred= np.array(radii_pred).reshape(512,512)
         tr_pred_tensor= torch.from_numpy(tr_pred)
         tcl_pred_tensor= torch.from_numpy(tcl_pred)
         tr_pred = tr_pred_tensor.softmax(dim=0).data.cpu().numpy()
         tcl_pred = tcl_pred_tensor.softmax(dim=0).data.cpu().numpy()
-        # if filename=="img95.jpg" or filename=="img565.jpg" or filename=="img992.jpg":
-        #     print("image",image,image.dtype)
-        #     print("tr",tr_pred,tr_pred.dtype)
-        #     print("tcl",tcl_pred,tcl_pred.dtype)
-        #     print("sin",sin_pred,sin_pred.dtype)
-        #     print("cos",cos_pred,cos_pred.dtype)
-        #     print("radii_pred",radii_pred,radii_pred.dtype)
-        # image= torch.from_numpy(image)#后面用的image都是tensor
         td=TextDetector(cfg.tr_thresh,cfg.tcl_thresh)
         contours = td.detect_contours(image, tr_pred, tcl_pred, sin_pred, cos_pred, radii_pred)
         output = {
@@ -203,22 +162,17 @@ if __name__ == '__main__':
             'cos': cos_pred,
             'radii': radii_pred
         }
-
-        #image = to_device(image)
         tr_pred, tcl_pred = output['tr'], output['tcl']
-        # idx=0 下面把image后面的[idx]也删掉了 这里batchsize相当于1
-        img_show = image.transpose(1,2,0)#.cpu().numpy()#问题：dataloader load过的东西全换成tensor了 后处理处理的全是tensor tensor的维度应该是3，512，512，但pipeline里处理的是512，512，3 所以出来的可能不是对应3，512，512的数据
-        img_show = ((img_show * cfg.stds + cfg.means) * 255).astype(np.uint8)
+        img_show = image.transpose(1,2,0)
+        img_show = ((img_show * stds + means) * 255).astype(np.uint8)
         img_show, contours = rescale_result(img_show, contours, H, W)
-        mkdirs(cfg.output_dir)
-        write_to_file(contours, os.path.join(cfg.output_dir, filename.replace('jpg', 'txt')))
+        mkdirs(cfg.output_dir+"/test")
+        write_to_file(contours, os.path.join(cfg.output_dir+"/test", filename.replace('jpg', 'txt')))
         pred_vis = visualize_detection(img_show, contours)
         cv2.imwrite(IMAGE_PATH1, pred_vis)
 
-    # destroy streams
-    # streamManagerApi.DestroyAllStreams()
     steam_manager_api.DestroyAllStreams()
     print('Computing DetEval in {}/{}'.format(cfg.output_dir, "test"))
-    subprocess.call(['python3.7', './Deteval.py', "test", '--tr', '0.7', '--tp', '0.6'])
-    subprocess.call(['python3.7', './Deteval.py', "test", '--tr', '0.8', '--tp', '0.4'])
+    subprocess.call(['python3.7', './util/Deteval.py', "test", '--tr', '0.7', '--tp', '0.6'])
+    subprocess.call(['python3.7', './util/Deteval.py', "test", '--tr', '0.8', '--tp', '0.4'])
     print('End.')
--- contrib/TextSnake/main.py
+++ contrib/TextSnake/main.py
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import json
 import os
 import cv2
@@ -24,22 +25,35 @@ import torch
 import stat
 import MxpiDataType_pb2 as MxpiDataType
 from StreamManagerApi import StreamManagerApi, MxProtobufIn, InProtobufVector, StringVector
-from misc import fill_hole, regularize_sin_cos
-from textdetector import TextDetector
-from misc import to_device, mkdirs, rescale_result
-from config import config as cfg
-from visualize import visualize_detection
-from imagereader import Resize,Normalize
+from util.misc import fill_hole, regularize_sin_cos
+from util.detection import TextDetector
+from util.misc import to_device, mkdirs, rescale_result
+from util.config import config as cfg
+from util.visualize import visualize_detection
+
+
+def norm(image,mean,std):
+    image = image.astype(np.float32)
+    image /= 255.0
+    image -= mean
+    image /= std
+    return image
+
+def resize(image,size):
+    h, w, _ = image.shape
+    image = cv2.resize(image, (size,size))
+    scales = np.array([size / w, size / h])
+    return image
 
 if __name__ == '__main__':
     steam_manager_api = StreamManagerApi()
-    # init stream manager
+    
     ret = steam_manager_api.InitManager()
     if ret != 0:
         print("Failed to init Stream manager, ret=%s" % str(ret))
         exit()
-    # input_shape = [416, 416]
-    # create streams by pipeline config file
+    
+    
     MODES = stat.S_IWUSR | stat.S_IRUSR
     with os.fdopen(os.open("./t.pipeline", os.O_RDONLY, MODES), 'rb') as f:
         pipeline_str = f.read()
@@ -48,21 +62,15 @@ if __name__ == '__main__':
         print("Failed to create Stream, ret=%s" % str(ret))
         exit()
 
+    means = (0.485, 0.456, 0.406)
+    stds = (0.229, 0.224, 0.225)
     IMAGE_PATH = './img1.jpg'
     image = Image.open(IMAGE_PATH)
     image = np.array(image)
     H, W, _ = image.shape
-    rsz=Resize(cfg.input_size)
-    image,_=rsz(image)
-    norm=Normalize(cfg.means,cfg.stds)
-    image,_=norm(image)
-    # success,image_b = cv2.imencode(".jpg",image)
+    image=resize(image,cfg.input_size)
+    image=norm(image,np.array(means),np.array(stds))
     image=image.transpose(2, 0, 1)
-    # image_b= image_b.transpose(2, 0, 1)
-    # img_bytes = image_b.tostring()
-    # image=Image.fromarray(image)
-    # image.save(IMAGE_PATH)
-    # cv2.imwrite(IMAGE_PATH1, image)
     visionList = MxpiDataType.MxpiVisionList()
     visionVec = visionList.visionVec.add()
 
@@ -81,8 +89,6 @@ if __name__ == '__main__':
     KEY0 = b"appsrc0"
 
     protobufVec = InProtobufVector()
-
-
     protobuf = MxProtobufIn()
     protobuf.key = KEY0
     protobuf.type = b"MxTools.MxpiVisionList"
@@ -98,8 +104,6 @@ if __name__ == '__main__':
     keyVec = StringVector()
     for key in keys:
         keyVec.push_back(key)
-    # 从流中取出对应插件的输出数据
-    #     infer = streamManagerApi.GetResult(STREAMNAME, b'appsink0', keyVec)
     infer = steam_manager_api.GetResult(STEAMNAME, b'appsink0', keyVec)
     if(infer.metadataVec.size() == 0):
         print("Get no data from stream !")
@@ -117,28 +121,10 @@ if __name__ == '__main__':
     sin_pred=pred_array[:,4,:,:].reshape(512,512)
     cos_pred=pred_array[:,5,:,:].reshape(512,512)
     radii_pred=pred_array[:,6,:,:].reshape(512,512)
-    # tr_pred = pred[0:524288]#0-524287
-    # tcl_pred = pred[524288:1048576]#524287-1048575
-    # sin_pred = pred[1048576:1310720]
-    # cos_pred = pred[1310720:1572864]
-    # radii_pred = pred[1572864:1835008]
-    # tr_pred= np.array(tr_pred).reshape(2,512,512)
-    # tcl_pred= np.array(tcl_pred).reshape(2,512,512)
-    # sin_pred= np.array(sin_pred).reshape(512,512)
-    # cos_pred= np.array(cos_pred).reshape(512,512)
-    # radii_pred= np.array(radii_pred).reshape(512,512)
     tr_pred_tensor= torch.from_numpy(tr_pred)
     tcl_pred_tensor= torch.from_numpy(tcl_pred)
     tr_pred = tr_pred_tensor.softmax(dim=0).data.cpu().numpy()
     tcl_pred = tcl_pred_tensor.softmax(dim=0).data.cpu().numpy()
-    # if filename=="img95.jpg" or filename=="img565.jpg" or filename=="img992.jpg":
-    #     print("image",image,image.dtype)
-    #     print("tr",tr_pred,tr_pred.dtype)
-    #     print("tcl",tcl_pred,tcl_pred.dtype)
-    #     print("sin",sin_pred,sin_pred.dtype)
-    #     print("cos",cos_pred,cos_pred.dtype)
-    #     print("radii_pred",radii_pred,radii_pred.dtype)
-    # image= torch.from_numpy(image)#后面用的image都是tensor
     td=TextDetector(cfg.tr_thresh,cfg.tcl_thresh)
     contours = td.detect_contours(image, tr_pred, tcl_pred, sin_pred, cos_pred, radii_pred)
     output = {
@@ -150,35 +136,14 @@ if __name__ == '__main__':
         'radii': radii_pred
     }
 
-    #image = to_device(image)
+    
     tr_pred, tcl_pred = output['tr'], output['tcl']
-    # idx=0 下面把image后面的[idx]也删掉了 这里batchsize相当于1
-    img_show = image.transpose(1,2,0)#.cpu().numpy()#问题：dataloader load过的东西全换成tensor了 后处理处理的全是tensor tensor的维度应该是3，512，512，但pipeline里处理的是512，512，3 所以出来的可能不是对应3，512，512的数据
-    img_show = ((img_show * cfg.stds + cfg.means) * 255).astype(np.uint8)
+    
+    img_show = image.transpose(1,2,0)
+    img_show = ((img_show * stds + means) * 255).astype(np.uint8)
     img_show, contours = rescale_result(img_show, contours, H, W)
     vis_dir = "result.jpg"
     pred_vis = visualize_detection(img_show, contours)
-    # print(pred_vis.shape)
+    mkdirs(vis_dir)
     cv2.imwrite(vis_dir, pred_vis)
-
-
-
-
-
-
-    # pred.resize(HEIGHT+1, WIDTH+1)
-    # preds = np.zeros((HEIGHT, WIDTH))
-    # for i in range(HEIGHT):
-    #     for j in range(WIDTH):
-    #         if(pred[i+1][j+1] < 0):
-    #             preds[i][j] = 0
-    #         elif(pred[i+1][j+1] > 1):
-    #             preds[i][j] = DE_NORM
-    #         else:
-    #             preds[i][j] = pred[i+1][j+1] * DE_NORM
-    # end_array = np.array(preds, dtype=int)
-    # SAVE_PATH = './result.jpg'
-    # img_resize = cv2.resize(end_array, (w, h), interpolation = cv2.INTER_NEAREST)
-    # cv2.imwrite(SAVE_PATH, img_resize)
-    # destroy streams
     steam_manager_api.DestroyAllStreams()
