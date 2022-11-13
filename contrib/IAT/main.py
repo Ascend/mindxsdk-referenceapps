3b848042305a224084013f685c049125e53e4d83
Dragonis <1677307600@qq.com>
2022-11-13 11:21:38 +0800

    check code

--- contrib/IAT/main.py
+++ contrib/IAT/main.py
@@ -5,11 +5,11 @@ from mindx.sdk import base
 from mindx.sdk.base import Tensor, Model, Size, log, ImageProcessor
 import cv2
 
-MODEL_PATH = "/home/nankaigcs1/IAT/models/iatsim.om"  #模型的路径
-IMAGE_PATH = "/home/nankaigcs1/IAT/data/eval15/low/1.png"  #输入图片
+MODEL_PATH = "/home/nankaigcs1/IAT/models/iatsim.om"   # 模型的路径
+IMAGE_PATH = "/home/nankaigcs1/IAT/data/eval15/low/1.png"   # 输入图片
 RESULT_PATH = "/home/nankaigcs1/IAT/data/result/"
 DATASET_DIR = "/home/nankaigcs1/IAT/data/eval15/"
-DEVICE_ID = 0  #芯片ID
+DEVICE_ID = 0   # 芯片ID
 
 
 class SSIM():
@@ -17,7 +17,7 @@ class SSIM():
     a class to evaluate SSIM
     """
     @staticmethod
-    def calc_SSIM(img1, img2):
+    def calc_ssim(img1, img2):
         ssim_c1 = (0.01 * 255)**2
         ssim_c2 = (0.03 * 255)**2
         img1 = img1.reshape(3, 400, 600).transpose(1, 2, 0)*255
@@ -42,7 +42,7 @@ class PSNR():
     a class to evaluate PSNR.
     """
     @staticmethod
-    def calc_PSNR(a, b):
+    def calc_psnr(a, b):
         mse = np.mean((a - b) ** 2)
         if mse == 0:
             return 0
@@ -70,16 +70,16 @@ def infer(image_path, is_save=False):
     :return: a numpy array of image
     """
     base.mx_init()
-    IAT = Model(MODEL_PATH, DEVICE_ID) #创造模型对象
+    model = Model(MODEL_PATH, DEVICE_ID)  #创造模型对象
 
     infer_image = get_image(image_path)
     #numpy to tensor
-    imageTensor = Tensor(infer_image)
-    imageTensor.to_device(DEVICE_ID)
-    input = [imageTensor]
+    image_tensor = Tensor(infer_image)
+    image_tensor.to_device(DEVICE_ID)
+    image_tensor = [image_tensor]
 
     #inference
-    outputs = IAT.infer(input)          #使用模型对Tensor对象进行推理
+    outputs = model.infer(image_tensor)  #使用模型对Tensor对象进行推理
     enhanced_img = outputs[0]
     enhanced_img.to_host()
     enhanced_img = np.array(enhanced_img)
@@ -104,8 +104,8 @@ def test_precision():
     for i in range(image_num):
         high_image = get_image(high_image_list[i])
         enhanced_image = infer(low_image_list[i])
-        psnr_sum += PSNR.calc_PSNR(high_image, enhanced_image)
-        ssim_sum += SSIM.calc_SSIM(high_image, enhanced_image)
+        psnr_sum += PSNR.calc_psnr(high_image, enhanced_image)
+        ssim_sum += SSIM.calc_ssim(high_image, enhanced_image)
 
     psnr_avg = psnr_sum / image_num
     ssim_avg = ssim_sum / image_num
@@ -114,11 +114,7 @@ def test_precision():
     return
 
 if __name__ == "__main__":
-
-    test_precision()
-    # try:
-    #     infer()
-    # except Exception as e:
-    #     print(e)
-
-    # test_tensor()
+    try:
+        infer(IMAGE_PATH)
+    except Exception as e:
+        print(e)
