de548368743162747cc197f1f08c261617bd8643
fr199787 <569835079@qq.com>
2022-11-14 11:58:53 +0800

    12:00 11-14

--- contrib/SuperResolutionV2/testSet/bmp2jpg.py
+++ contrib/SuperResolutionV2/testSet/bmp2jpg.py
@@ -15,24 +15,24 @@ from PIL import Image
 
 if __name__ == '__main__':
     # test image set path
-    test_image_set_path = "Set14"
+    TEST_IMAGE_SET_PATH = "Set14"
     # parse command arguments
     if len(sys.argv) == 2:
         if sys.argv[1] == '':
             print('test image set path is not valid, use default config.')
         else:
-            test_image_set_path = sys.argv[1]
+            TEST_IMAGE_SET_PATH = sys.argv[1]
     # jpg format image set path
-    output_image_set_path = test_image_set_path + "-jpg"
+    output_image_set_path = TEST_IMAGE_SET_PATH + "-jpg"
     # check output paths
     if os.path.exists(output_image_set_path) != 1:
         print('The image set path {} does not exist, create it.'.format(output_image_set_path))
         os.mkdir(output_image_set_path)
 
     # get all image files
-    image_files = os.listdir(test_image_set_path)
+    image_files = os.listdir(TEST_IMAGE_SET_PATH)
     # convert bmp to jpg
     for test_image_path in image_files:
-        image_file = test_image_set_path+"/" + test_image_path
+        image_file = TEST_IMAGE_SET_PATH + "/" + test_image_path
         out_img = Image.open(image_file).convert("RGB")
         out_img.save(output_image_set_path + "/" + test_image_path[:-4] + ".jpg")
