4d0f3ffc0d062b34f1bf8b45e9f28e67f7be648c
fr199787 <569835079@qq.com>
2022-11-14 16:41:51 +0800

    16：40 11-14

--- contrib/SuperResolutionV2/model/model_conversion.sh
+++ contrib/SuperResolutionV2/model/model_conversion.sh
@@ -23,12 +23,6 @@ set -e
 # 设置环境变量（请确认install_path路径是否正确）
 # Set environment PATH (Please confirm that the install_path is correct).
 
-export install_path=/usr/local/Ascend/ascend-toolkit/latest
-export PATH=/usr/local/python3.9.2/bin:${install_path}/atc/ccec_compiler/bin:${install_path}/atc/bin:$PATH
-export PYTHONPATH=${install_path}/atc/python/site-packages:${install_path}/atc/python/site-packages/auto_tune.egg/auto_tune:${install_path}/atc/python/site-packages/schedule_search.egg
-export LD_LIBRARY_PATH=${install_path}/atc/lib64:$LD_LIBRARY_PATH
-export ASCEND_OPP_PATH=${install_path}/opp
-
 
 # 执行，转换VDSR模型
 # Execute, transform VDSR model.
