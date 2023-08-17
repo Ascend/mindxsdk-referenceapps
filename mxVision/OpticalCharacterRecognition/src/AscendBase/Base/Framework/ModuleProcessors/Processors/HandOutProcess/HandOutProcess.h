f0110cf2fa748167f3f5f9d59f24873665fd25ac
bilei <bilei5@huawei.com>
2023-08-17 12:45:12 +0800

    commit the HandOutProcess/HandOutProcess.h

---
+++ mxVision/OpticalCharacterRecognition/src/AscendBase/Base/Framework/ModuleProcessors/Processors/HandOutProcess/HandOutProcess.h
@@ -0,0 +1,31 @@
+#ifndef CPP_HANDOUTPROCESS_H
+#define CPP_HANDOUTPROCESS_H
+
+#include "ModuleManagers/ModuleManager.h"
+#include "ConfigParser/ConfigParser.h"
+#include "CommonData/CommonData.h"
+#include "Utils.h"
+#include "Signal.h"
+#include "ErrorCode/ErrorCode.h"
+#include "Log/Log.h"
+
+class HandoutProcess : public ascendOCR::ModuleBase {
+public:
+	HandoutProcess();
+	~HandoutProcess();
+	APP_ERROR Init(ConfigParser &configParser, ascendOCR::ModuleInitParams &initParams);
+	APP_ERROR DeInit(void);
+
+protected:
+	APP_ERROR Process(std::shared_ptr<void> inputData);
+
+private:
+	int imgId_ = 0;
+	std::string deviceType_;
+	APP_ERROR ParseConfig(ConfigParser &configParser);
+	bool saveInferResult;
+	std::string resultPath;
+};
+MODULE_REGIST(HandOutProcess)
+#endif
+
