00b47a354904367089426306819236f3132a1171
taoqiuyu <taoqiuyu2@huawei.com>
2023-08-17 16:21:49 +0800

    add argumentParser blockingqueue configparser

---
+++ mxVision/OpticalCharacterRecognition/src/AscendBase/Base/ArgumentParser/ArgumentParser.h
@@ -0,0 +1,48 @@
+/*
+* Copyright (c) 2020 Huawei Technologies Co., All rights reserved.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef ARGUMENT_PARSER_H
+#define ARGUMENT_PARSER_H
+
+#include <string>
+#include <map>
+#include <vector>
+#include "Log/Log.h"
+
+// Argument parser class
+class ArgumentParser {
+public:
+    ArgumentParser();
+    ArgumentParser(int argc, const char **argv);
+    ~ArgumentParser() {};
+    // Add arguments into the map
+    void AddArgument(const std::string &argument, const std::string &defaults = "", const std::string &message="");
+    // Parse the input arguments
+    void ParseArgs(int argc, const char **argv);
+    // Get the string argument value
+    const std::string &GetStringArgumentValue(const std::string &argumentValue);
+    // Get the int argument value
+    const std::int GetIntArgumentValue(const std::string &argumentValue);
+    // Get the bool argument value
+    const std::bool GetBoolArgumentValue(const std::string &argumentValue);
+
+private:
+    std::map<std::string, std::pair<std::string, std::string>> argumentMap_;
+    // show the help information, then exit
+    void DisplayHelpInformation() const;
+    bool IsInteger(std::string &str) const;
+    bool IsDecimal(std::string &str) const;
+};
+#endif
