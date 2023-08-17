f92840001e9cef8ab385d0be26aec8b033057495
bilei <bilei5@huawei.com>
2023-08-17 12:34:39 +0800

    modified the tab

--- mxVision/OpticalCharacterRecognition/src/AscendBase/Base/Framework/ModuleManagers/ModuleFactory.h
+++ mxVision/OpticalCharacterRecognition/src/AscendBase/Base/Framework/ModuleManagers/ModuleFactory.h
@@ -5,42 +5,42 @@
 #include <map>
 #include <functional>
 
-#define MODULE_REGIST(class_name)														  \
-	namespace ascendOCR {															  \
-	class class_name##Helper {															  \
-	public:																				  \
-		class_name##Helper()															  \
-		{																				  \
-			ModuleFactory::RegisterModule(#class_name, class_name##Helper::CreatObjFunc); \
-		}																				  \
-		static class_name##Helper class_name##helper;									  \
-		static std::string MT_##class_name = #class_name;								  \
-		}
+#define MODULE_REGIST(class_name)                                                         \
+    namespace ascendOCR {                                                                 \
+    class class_name##Helper {                                                            \
+    public:                                                                               \
+        class_name##Helper()                                                              \
+        {                                                                                 \
+            ModuleFactory::RegisterModule(#class_name, class_name##Helper::CreatObjFunc); \
+        }                                                                                 \
+        static class_name##Helper class_name##helper;                                     \
+        static std::string MT_##class_name = #class_name;                                 \
+        }
 namespace ascendOCR {
-	using Constructor = std::function<void *()>;
-	class ModuleFactory
-	{
-	public:
-		static void RegisterModule(std::string className, Constructor constructor)
-		{
-			Constructors()[className] = constructor;
-		}
+    using Constructor = std::function<void *()>;
+    class ModuleFactory
+    {
+    public:
+        static void RegisterModule(std::string className, Constructor constructor)
+        {
+            Constructors()[className] = constructor;
+        }
 
-		static void *MakeModule(const std::string &className)
-		{
-			auto itr = Constructors().find(className);
-			if (itr == Constructors().end()) {
-				return nullptr;
-			}
-			return ((Constructor)itr->second)();
-		}
-	private:
-		inline static std::map<std::string, Constructor> &Constructors()
-		{
-			static std::map<std::string, Constructor> instance;
-			return instance;
-		}
-	};
+        static void *MakeModule(const std::string &className)
+        {
+            auto itr = Constructors().find(className);
+            if (itr == Constructors().end()) {
+                return nullptr;
+            }
+            return ((Constructor)itr->second)();
+        }
+    private:
+        inline static std::map<std::string, Constructor> &Constructors()
+        {
+            static std::map<std::string, Constructor> instance;
+            return instance;
+        }
+    };
 }
 
 #endif
